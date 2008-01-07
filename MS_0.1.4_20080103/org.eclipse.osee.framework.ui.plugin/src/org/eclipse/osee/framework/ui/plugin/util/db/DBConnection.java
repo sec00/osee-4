4e7af86e311ef86ff4f597f18cf9789563e7c8bb
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-01-07 17:51:02 +0000

    XBSGY - Activate osee_log.version check when launch OSEE to ensure db/release compatibility

--- MS_0.1.4_20080103/org.eclipse.osee.framework.ui.plugin/src/org/eclipse/osee/framework/ui/plugin/util/db/DBConnection.java
+++ MS_0.1.4_20080103/org.eclipse.osee.framework.ui.plugin/src/org/eclipse/osee/framework/ui/plugin/util/db/DBConnection.java
@@ -11,9 +11,7 @@
 package org.eclipse.osee.framework.ui.plugin.util.db;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,14 +20,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.osee.framework.jdk.core.util.OseeProperties;
 import org.eclipse.osee.framework.plugin.core.PluginCoreActivator;
 import org.eclipse.osee.framework.plugin.core.config.ConfigUtil;
 import org.eclipse.osee.framework.plugin.core.config.data.DbInformation;
 import org.eclipse.osee.framework.plugin.core.config.data.DbDetailData.ConfigField;
 import org.eclipse.osee.framework.plugin.core.db.IConnection;
 import org.eclipse.osee.framework.plugin.core.util.ExtensionPoints;
-import org.eclipse.osee.framework.ui.plugin.util.OseeInfo;
+import org.eclipse.osee.framework.ui.plugin.util.OseeDbVersion;
 import org.osgi.framework.Bundle;
 
 /**
@@ -37,8 +34,6 @@ import org.osgi.framework.Bundle;
  */
 public class DBConnection {
    private static final Logger logger = ConfigUtil.getConfigFactory().getLogger(DBConnection.class);
-   public static final int CODE_VERSION_VALUE = 20080103;
-   public static final String DB_VERSION_KEY = "version";
 
    private static Map<String, IConnection> connections = new HashMap<String, IConnection>();
 
@@ -77,7 +72,7 @@ public class DBConnection {
 
          if (validityCheck && !userName.equals("peer")) {
             try {
-               ensureDatabaseCompatability(connection);
+               OseeDbVersion.ensureDatabaseCompatability(connection);
             } catch (Exception ex) {
                connection.close();
                throw ex;
@@ -113,47 +108,4 @@ public class DBConnection {
       }
    }
 
-   private static String dbVersionStr = null;
-
-   public static String getDatabaseVersion(Connection connection) throws SQLException {
-      // NOTE: Can't use OSEEInfo class cause it uses connection handler
-      String query = "SELECT OSEE_VALUE FROM OSEE_INFO WHERE OSEE_KEY = \'version\'";
-
-      Statement statement = connection.createStatement();
-      statement.setFetchSize(1);
-      ResultSet rset = statement.executeQuery(query);
-      if (rset.next()) {
-         return rset.getString("OSEE_VALUE");
-      }
-      statement.close();
-      return "";
-   }
-
-   private static void ensureDatabaseCompatability(Connection connection) throws Exception {
-      if (dbVersionStr == null) {
-         dbVersionStr = getDatabaseVersion(connection);
-      }
-      int dbVersionInt = 0;
-      if (dbVersionStr != null) {
-         try {
-            dbVersionInt = new Integer(dbVersionStr);
-         } catch (Exception ex) {
-            // do nothing
-         }
-      }
-      if (dbVersionStr == null || CODE_VERSION_VALUE < dbVersionInt) {
-         String errorStr =
-               String.format(
-                     "This installation of OSEE \"%d\" is out of date and is not compatible with database version \"%s\".  Please restart OSEE and accept all updates.",
-                     CODE_VERSION_VALUE, dbVersionStr);
-         if (!OseeProperties.getInstance().isOverrideVersionCheck())
-            throw new IllegalArgumentException(errorStr);
-         else
-            logger.log(Level.SEVERE, "Overriding Version Check - " + errorStr);
-      }
-   }
-
-   public static void insertDbVersion() throws SQLException {
-      OseeInfo.putValue(DB_VERSION_KEY, String.valueOf(CODE_VERSION_VALUE));
-   }
 }
