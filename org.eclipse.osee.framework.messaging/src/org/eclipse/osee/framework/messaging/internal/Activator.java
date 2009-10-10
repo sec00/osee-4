3af2e42ec8d8ea14602b23a11439e76c0705a64b
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-10-10 22:13:12 +0000

    Milestone Branch for 0.8.2

---
+++ org.eclipse.osee.framework.messaging/src/org/eclipse/osee/framework/messaging/internal/Activator.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.messaging.internal;
+
+import java.util.Hashtable;
+import org.eclipse.osee.framework.messaging.MessagingGateway;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Andrew M. Finkbeiner
+ */
+public class Activator implements BundleActivator {
+
+   private ServiceRegistration registration;
+   private MessagingGatewayImpl messaging;
+
+   public void start(BundleContext context) throws Exception {
+      messaging = new MessagingGatewayImpl();
+      registration = context.registerService(MessagingGateway.class.getName(), messaging, new Hashtable());
+   }
+
+   public void stop(BundleContext context) throws Exception {
+      if (registration != null) {
+         registration.unregister();
+      }
+
+      if (messaging != null) {
+         messaging.dispose();
+      }
+   }
+}
