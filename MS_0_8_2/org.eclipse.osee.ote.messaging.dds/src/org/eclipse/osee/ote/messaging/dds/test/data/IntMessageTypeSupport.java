acbdd94b16de72381567bd4955aa0c62c628f408
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-10-10 22:09:46 +0000

    Milestone Branch for 0.8.2

---
+++ MS_0_8_2/org.eclipse.osee.ote.messaging.dds/src/org/eclipse/osee/ote/messaging/dds/test/data/IntMessageTypeSupport.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.ote.messaging.dds.test.data;
+import org.eclipse.osee.ote.messaging.dds.service.Key;
+import org.eclipse.osee.ote.messaging.dds.service.TypeSupport;
+
+/**
+ * @author Robert A. Fisher
+ * @author David Diepenbrock
+ */
+public class IntMessageTypeSupport extends TypeSupport {
+   
+   protected int getTypeDataSize() {
+      return Integer.SIZE/8;
+   }
+
+   protected Key getKey() {
+      return null;
+   }
+
+   protected String getReaderName() {
+      return IntMessageReader.class.getCanonicalName();
+   }
+
+   protected String getWriterName() {
+      return IntMessageWriter.class.getCanonicalName();
+   }
+
+}
