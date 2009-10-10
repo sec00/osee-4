96c2573e1dccaaa0bfd6316419d236bda340b0fe
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-10-10 22:12:53 +0000

    Milestone Branch for 0.8.2

---
+++ org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/enums/ModificationType.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.osee.framework.core.enums;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.eclipse.osee.framework.core.exception.OseeArgumentException;
+
+/**
+ * @author Ryan D. Brooks
+ */
+public enum ModificationType implements Serializable {
+   // Artifact, Attribute or Relation that was newly created
+   NEW("New", 1),
+
+   // Artifact if any Attribute was changed (not Relation)
+   // Attribute if it's value was modified
+   // Relation if rationale or ordering change
+   MODIFIED("Modified", 2),
+
+   // Artifact, Attribute or Relation was deleted
+   DELETED("Deleted", 3),
+
+   // Artifact: Not Valid
+   // Attribute: if value was merged from merge manager
+   // TODO Relation: Not Currently Valid, but needs to be handled
+   MERGED("Merged", 4),
+
+   // Artifact: Not Valid
+   // Attribute or Relation: was deleted as a direct result of Artifact delete, will be marked as ARTIFACT_DELETED
+   ARTIFACT_DELETED("Artifact Deleted", 5),
+
+   // Artifact, Attribute or Relation has been reflected from another branch
+   INTRODUCED("Introduced", 6);
+
+   private final static Set<ModificationType> ALL_NONE_HARD_DELETED = new HashSet<ModificationType>();
+   private final static Set<ModificationType> ALL_MOD_TYPES = new HashSet<ModificationType>();
+   private final static Set<ModificationType> ALL_STATES = new HashSet<ModificationType>();
+   private int value;
+   private String displayName;
+
+   ModificationType(String displayName, int value) {
+      this.displayName = displayName;
+      this.value = value;
+   }
+
+   /**
+    * @return Returns the value.
+    */
+   public int getValue() {
+      return value;
+   }
+
+   public String getDisplayName() {
+      return displayName;
+   }
+
+   /**
+    * @param value The value of the ModificationType to get.
+    * @return The ModificationType that has the value passed.
+    * @throws OseeArgumentException
+    */
+   public static ModificationType getMod(int value) throws OseeArgumentException {
+      for (ModificationType modtype : values()) {
+         if (modtype.value == value) {
+            return modtype;
+         }
+      }
+      throw new OseeArgumentException(value + " does not correspond to any defined ModificationType enumerations");
+   }
+
+   public boolean isDeleted() {
+      return this == DELETED || this == ARTIFACT_DELETED;
+   }
+
+   public static Set<ModificationType> getCurrentModTypes() {
+      if (ALL_NONE_HARD_DELETED.isEmpty()) {
+         synchronized (ALL_NONE_HARD_DELETED) {
+            ALL_NONE_HARD_DELETED.addAll(Arrays.asList(ModificationType.values()));
+            ALL_NONE_HARD_DELETED.remove(ModificationType.DELETED);
+         }
+      }
+      return ALL_NONE_HARD_DELETED;
+   }
+
+   public static Set<ModificationType> getAllCurrentModTypes() {
+      if (ALL_MOD_TYPES.isEmpty()) {
+         synchronized (ALL_MOD_TYPES) {
+            ALL_MOD_TYPES.addAll(Arrays.asList(ModificationType.values()));
+         }
+      }
+      return ALL_MOD_TYPES;
+   }
+
+   public static Set<ModificationType> getAllStates() {
+      if (ALL_STATES.isEmpty()) {
+         synchronized (ALL_STATES) {
+            ALL_STATES.addAll(Arrays.asList(ModificationType.values()));
+         }
+      }
+      return ALL_STATES;
+   }
+}
---
+++ org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/operation/AbstractOperation.java
@@ -0,0 +1,224 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.core.operation;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public abstract class AbstractOperation implements IOperation {
+
+   private final InternalMultiStatus status;
+   private boolean wasExecuted;
+   private String name;
+
+   /**
+    * @param operationName
+    * @param pluginId
+    */
+   public AbstractOperation(String operationName, String pluginId) {
+      this.status = new InternalMultiStatus(pluginId, IStatus.OK, operationName);
+      this.wasExecuted = false;
+      setName(operationName);
+   }
+
+   /**
+    * Sets the operation's name
+    * 
+    * @param name
+    */
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public IStatus getStatus() {
+      return status;
+   }
+
+   /**
+    * Updates the operation's status
+    * 
+    * @param status
+    */
+   protected void setStatus(IStatus status) {
+      if (status.getSeverity() != IStatus.OK) {
+         this.status.merge(status);
+      }
+   }
+
+   public boolean wasExecuted() {
+      return wasExecuted;
+   }
+
+   public final IOperation run(IProgressMonitor monitor) {
+      wasExecuted = true;
+      try {
+         doWork(monitor);
+         checkForCancelledStatus(monitor);
+      } catch (Throwable error) {
+         setStatus(createErrorStatus(error));
+      } finally {
+         doFinally(monitor);
+      }
+      return this;
+   }
+
+   /**
+    * Convenience method to allow clients to hook into the operation's finally block
+    * 
+    * @param monitor
+    */
+   protected void doFinally(IProgressMonitor monitor) {
+
+   }
+
+   /**
+    * All work should be performed here
+    * 
+    * @param monitor
+    * @throws Exception
+    */
+   protected abstract void doWork(IProgressMonitor monitor) throws Exception;
+
+   /**
+    * Updates the operation's status message
+    * 
+    * @param message
+    */
+   protected void setStatusMessage(String message) {
+      status.setMessage(message);
+   }
+
+   /**
+    * Creates a new error status object
+    * 
+    * @param error
+    * @return
+    */
+   protected IStatus createErrorStatus(Throwable error) {
+      if (error instanceof OperationCanceledException) {
+         return Status.CANCEL_STATUS;
+      } else {
+         return new Status(IStatus.ERROR, status.getPlugin(), String.format("%s: %s", status.getMessage(),
+               error.getLocalizedMessage()), error);
+      }
+   }
+
+   /**
+    * @param workPercentage
+    * @return
+    */
+   protected int calculateWork(double workPercentage) {
+      return Operations.calculateWork(getTotalWorkUnits(), workPercentage);
+   }
+
+   /**
+    * Executes a nested operation setting monitor begin and done. If workPercentage is set greater than 0, monitor will
+    * be wrapped into a SubProgressMonitor set to the appropriate number of ticks to consume from the main monitor.
+    * Checks for status after work is complete to detect for execution errors or canceled.
+    * 
+    * @param operation
+    * @param monitor
+    * @param workPercentage
+    * @throws Exception
+    */
+   public void doSubWork(IOperation operation, IProgressMonitor monitor, double workPercentage) throws Exception {
+      doSubWorkNoChecks(operation, monitor, workPercentage);
+      checkForErrorsOrCanceled(monitor);
+   }
+
+   /**
+    * Executes a nested operation setting monitor begin and done. If workPercentage is set greater than 0, monitor will
+    * be wrapped into a SubProgressMonitor set to the appropriate number of ticks to consume from the main monitor.
+    * Clients should use {@link #doSubWork(IOperation, IProgressMonitor, double)} when required to throw exceptions for
+    * status errors or canceled. Alternatively, clients can perform the appropriate checks after calling this method.
+    * The operation's status contains the result of having executed the sub-operation.
+    * 
+    * @param operation
+    * @param monitor
+    * @param workPercentage
+    */
+   public void doSubWorkNoChecks(IOperation operation, IProgressMonitor monitor, double workPercentage) {
+      Operations.executeWork(operation, monitor, workPercentage);
+      setStatus(operation.getStatus());
+   }
+
+   /**
+    * Throws an exception if the severity mask is detected.
+    * 
+    * @param monitor
+    * @throws Exception
+    */
+   protected void checkForStatusSeverityMask(int severityMask) throws Exception {
+      Operations.checkForStatusSeverityMask(getStatus(), severityMask);
+   }
+
+   /**
+    * Checks that the user has not canceled the operation and that the operation's status is still OK. If the status has
+    * changed to ERROR, WARNING or CANCEL - an Exception will be thrown.
+    * 
+    * @param monitor
+    * @throws Exception
+    */
+   protected void checkForErrorsOrCanceled(IProgressMonitor monitor) throws Exception {
+      checkForCancelledStatus(monitor);
+      Operations.checkForErrorStatus(getStatus());
+   }
+
+   /**
+    * Checks to see if the user cancelled the operation. If the operation was cancelled, the method will throw an
+    * OperationCanceledException
+    * 
+    * @param monitor
+    * @throws OperationCanceledException
+    */
+   protected void checkForCancelledStatus(IProgressMonitor monitor) throws OperationCanceledException {
+      if (monitor.isCanceled()) {
+         boolean wasCancelled = false;
+         IStatus[] children = this.status.getChildren();
+         for (int i = 0; i < children.length; i++) {
+            Throwable exception = children[i].getException();
+            if (exception instanceof OperationCanceledException) {
+               wasCancelled = true;
+               break;
+            }
+         }
+         if (!wasCancelled) {
+            throw new OperationCanceledException("Cancelled");
+         }
+      }
+   }
+
+   @Override
+   public int getTotalWorkUnits() {
+      return IOperation.TOTAL_WORK;
+   }
+
+   private final static class InternalMultiStatus extends org.eclipse.core.runtime.MultiStatus {
+
+      public InternalMultiStatus(String pluginId, int code, String message) {
+         super(pluginId, code, message, null);
+      }
+
+      @Override
+      public void setMessage(String message) {
+         super.setMessage(message);
+      }
+   }
+}
