1b497a4e437b6b4fbcb12a0b7211e2fed7b26123
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-10-10 22:15:28 +0000

    Milestone Branch for 0.8.2

---
+++ org.eclipse.osee.framework.search.engine/src/org/eclipse/osee/framework/search/engine/tagger/XmlAttributeTaggerProvider.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.search.engine.tagger;
+
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import org.eclipse.osee.framework.jdk.core.util.Strings;
+import org.eclipse.osee.framework.jdk.core.util.io.xml.XmlTextInputStream;
+import org.eclipse.osee.framework.search.engine.MatchLocation;
+import org.eclipse.osee.framework.search.engine.SearchOptions;
+import org.eclipse.osee.framework.search.engine.attribute.AttributeData;
+import org.eclipse.osee.framework.search.engine.utility.ITagCollector;
+import org.eclipse.osee.framework.search.engine.utility.TagProcessor;
+import org.eclipse.osee.framework.search.engine.utility.WordOrderMatcher;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public class XmlAttributeTaggerProvider extends BaseAttributeTaggerProvider {
+
+   @Override
+   public List<MatchLocation> find(AttributeData attributeData, String toSearch, SearchOptions options) throws Exception {
+      if (Strings.isValid(toSearch)) {
+         InputStream inputStream = null;
+         try {
+            inputStream = new XmlTextInputStream(getValueAsStream(attributeData));
+            return WordOrderMatcher.findInStream(inputStream, toSearch, options);
+         } finally {
+            if (inputStream != null) {
+               inputStream.close();
+            }
+         }
+      }
+      return Collections.emptyList();
+   }
+
+   public void tagIt(AttributeData attributeData, ITagCollector collector) throws Exception {
+      InputStream inputStream = null;
+      try {
+         inputStream = getValueAsStream(attributeData);
+         TagProcessor.collectFromInputStream(new XmlTextInputStream(inputStream), collector);
+      } finally {
+         if (inputStream != null) {
+            inputStream.close();
+         }
+      }
+   }
+}
