d5a824c238dc9e2cad401bffe87748bd4132349c
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-10-10 22:22:37 +0000

    Milestone Branch for 0.8.2

---
+++ org.eclipse.osee.framework.types/src-gen/org/eclipse/osee/framework/oseeTypes/impl/OseeTypeModelImpl.java
@@ -0,0 +1,366 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.eclipse.osee.framework.oseeTypes.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import org.eclipse.osee.framework.oseeTypes.ArtifactType;
+import org.eclipse.osee.framework.oseeTypes.AttributeType;
+import org.eclipse.osee.framework.oseeTypes.Import;
+import org.eclipse.osee.framework.oseeTypes.OseeEnumOverride;
+import org.eclipse.osee.framework.oseeTypes.OseeEnumType;
+import org.eclipse.osee.framework.oseeTypes.OseeTypeModel;
+import org.eclipse.osee.framework.oseeTypes.OseeTypesPackage;
+import org.eclipse.osee.framework.oseeTypes.RelationType;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Osee Type Model</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.eclipse.osee.framework.oseeTypes.impl.OseeTypeModelImpl#getImports <em>Imports</em>}</li>
+ *   <li>{@link org.eclipse.osee.framework.oseeTypes.impl.OseeTypeModelImpl#getArtifactTypes <em>Artifact Types</em>}</li>
+ *   <li>{@link org.eclipse.osee.framework.oseeTypes.impl.OseeTypeModelImpl#getRelationTypes <em>Relation Types</em>}</li>
+ *   <li>{@link org.eclipse.osee.framework.oseeTypes.impl.OseeTypeModelImpl#getAttributeTypes <em>Attribute Types</em>}</li>
+ *   <li>{@link org.eclipse.osee.framework.oseeTypes.impl.OseeTypeModelImpl#getEnumTypes <em>Enum Types</em>}</li>
+ *   <li>{@link org.eclipse.osee.framework.oseeTypes.impl.OseeTypeModelImpl#getEnumOverrides <em>Enum Overrides</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class OseeTypeModelImpl extends MinimalEObjectImpl.Container implements OseeTypeModel
+{
+  /**
+   * The cached value of the '{@link #getImports() <em>Imports</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getImports()
+   * @generated
+   * @ordered
+   */
+  protected EList<Import> imports;
+
+  /**
+   * The cached value of the '{@link #getArtifactTypes() <em>Artifact Types</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getArtifactTypes()
+   * @generated
+   * @ordered
+   */
+  protected EList<ArtifactType> artifactTypes;
+
+  /**
+   * The cached value of the '{@link #getRelationTypes() <em>Relation Types</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getRelationTypes()
+   * @generated
+   * @ordered
+   */
+  protected EList<RelationType> relationTypes;
+
+  /**
+   * The cached value of the '{@link #getAttributeTypes() <em>Attribute Types</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getAttributeTypes()
+   * @generated
+   * @ordered
+   */
+  protected EList<AttributeType> attributeTypes;
+
+  /**
+   * The cached value of the '{@link #getEnumTypes() <em>Enum Types</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getEnumTypes()
+   * @generated
+   * @ordered
+   */
+  protected EList<OseeEnumType> enumTypes;
+
+  /**
+   * The cached value of the '{@link #getEnumOverrides() <em>Enum Overrides</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getEnumOverrides()
+   * @generated
+   * @ordered
+   */
+  protected EList<OseeEnumOverride> enumOverrides;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected OseeTypeModelImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return OseeTypesPackage.Literals.OSEE_TYPE_MODEL;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<Import> getImports()
+  {
+    if (imports == null)
+    {
+      imports = new EObjectContainmentEList<Import>(Import.class, this, OseeTypesPackage.OSEE_TYPE_MODEL__IMPORTS);
+    }
+    return imports;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<ArtifactType> getArtifactTypes()
+  {
+    if (artifactTypes == null)
+    {
+      artifactTypes = new EObjectContainmentEList<ArtifactType>(ArtifactType.class, this, OseeTypesPackage.OSEE_TYPE_MODEL__ARTIFACT_TYPES);
+    }
+    return artifactTypes;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<RelationType> getRelationTypes()
+  {
+    if (relationTypes == null)
+    {
+      relationTypes = new EObjectContainmentEList<RelationType>(RelationType.class, this, OseeTypesPackage.OSEE_TYPE_MODEL__RELATION_TYPES);
+    }
+    return relationTypes;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<AttributeType> getAttributeTypes()
+  {
+    if (attributeTypes == null)
+    {
+      attributeTypes = new EObjectContainmentEList<AttributeType>(AttributeType.class, this, OseeTypesPackage.OSEE_TYPE_MODEL__ATTRIBUTE_TYPES);
+    }
+    return attributeTypes;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<OseeEnumType> getEnumTypes()
+  {
+    if (enumTypes == null)
+    {
+      enumTypes = new EObjectContainmentEList<OseeEnumType>(OseeEnumType.class, this, OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_TYPES);
+    }
+    return enumTypes;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<OseeEnumOverride> getEnumOverrides()
+  {
+    if (enumOverrides == null)
+    {
+      enumOverrides = new EObjectContainmentEList<OseeEnumOverride>(OseeEnumOverride.class, this, OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_OVERRIDES);
+    }
+    return enumOverrides;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+  {
+    switch (featureID)
+    {
+      case OseeTypesPackage.OSEE_TYPE_MODEL__IMPORTS:
+        return ((InternalEList<?>)getImports()).basicRemove(otherEnd, msgs);
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ARTIFACT_TYPES:
+        return ((InternalEList<?>)getArtifactTypes()).basicRemove(otherEnd, msgs);
+      case OseeTypesPackage.OSEE_TYPE_MODEL__RELATION_TYPES:
+        return ((InternalEList<?>)getRelationTypes()).basicRemove(otherEnd, msgs);
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ATTRIBUTE_TYPES:
+        return ((InternalEList<?>)getAttributeTypes()).basicRemove(otherEnd, msgs);
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_TYPES:
+        return ((InternalEList<?>)getEnumTypes()).basicRemove(otherEnd, msgs);
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_OVERRIDES:
+        return ((InternalEList<?>)getEnumOverrides()).basicRemove(otherEnd, msgs);
+    }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case OseeTypesPackage.OSEE_TYPE_MODEL__IMPORTS:
+        return getImports();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ARTIFACT_TYPES:
+        return getArtifactTypes();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__RELATION_TYPES:
+        return getRelationTypes();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ATTRIBUTE_TYPES:
+        return getAttributeTypes();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_TYPES:
+        return getEnumTypes();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_OVERRIDES:
+        return getEnumOverrides();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case OseeTypesPackage.OSEE_TYPE_MODEL__IMPORTS:
+        getImports().clear();
+        getImports().addAll((Collection<? extends Import>)newValue);
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ARTIFACT_TYPES:
+        getArtifactTypes().clear();
+        getArtifactTypes().addAll((Collection<? extends ArtifactType>)newValue);
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__RELATION_TYPES:
+        getRelationTypes().clear();
+        getRelationTypes().addAll((Collection<? extends RelationType>)newValue);
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ATTRIBUTE_TYPES:
+        getAttributeTypes().clear();
+        getAttributeTypes().addAll((Collection<? extends AttributeType>)newValue);
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_TYPES:
+        getEnumTypes().clear();
+        getEnumTypes().addAll((Collection<? extends OseeEnumType>)newValue);
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_OVERRIDES:
+        getEnumOverrides().clear();
+        getEnumOverrides().addAll((Collection<? extends OseeEnumOverride>)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case OseeTypesPackage.OSEE_TYPE_MODEL__IMPORTS:
+        getImports().clear();
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ARTIFACT_TYPES:
+        getArtifactTypes().clear();
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__RELATION_TYPES:
+        getRelationTypes().clear();
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ATTRIBUTE_TYPES:
+        getAttributeTypes().clear();
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_TYPES:
+        getEnumTypes().clear();
+        return;
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_OVERRIDES:
+        getEnumOverrides().clear();
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case OseeTypesPackage.OSEE_TYPE_MODEL__IMPORTS:
+        return imports != null && !imports.isEmpty();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ARTIFACT_TYPES:
+        return artifactTypes != null && !artifactTypes.isEmpty();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__RELATION_TYPES:
+        return relationTypes != null && !relationTypes.isEmpty();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ATTRIBUTE_TYPES:
+        return attributeTypes != null && !attributeTypes.isEmpty();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_TYPES:
+        return enumTypes != null && !enumTypes.isEmpty();
+      case OseeTypesPackage.OSEE_TYPE_MODEL__ENUM_OVERRIDES:
+        return enumOverrides != null && !enumOverrides.isEmpty();
+    }
+    return super.eIsSet(featureID);
+  }
+
+} //OseeTypeModelImpl
---
+++ org.eclipse.osee.framework.types/src-gen/org/eclipse/osee/framework/oseeTypes/util/OseeTypesSwitch.java
@@ -0,0 +1,453 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.eclipse.osee.framework.oseeTypes.util;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+
+import org.eclipse.osee.framework.oseeTypes.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Switch</b> for the model's inheritance hierarchy.
+ * It supports the call {@link #doSwitch(EObject) doSwitch(object)}
+ * to invoke the <code>caseXXX</code> method for each class of the model,
+ * starting with the actual class of the object
+ * and proceeding up the inheritance hierarchy
+ * until a non-null result is returned,
+ * which is the result of the switch.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.osee.framework.oseeTypes.OseeTypesPackage
+ * @generated
+ */
+public class OseeTypesSwitch<T>
+{
+  /**
+   * The cached model package
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected static OseeTypesPackage modelPackage;
+
+  /**
+   * Creates an instance of the switch.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public OseeTypesSwitch()
+  {
+    if (modelPackage == null)
+    {
+      modelPackage = OseeTypesPackage.eINSTANCE;
+    }
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
+  public T doSwitch(EObject theEObject)
+  {
+    return doSwitch(theEObject.eClass(), theEObject);
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
+  protected T doSwitch(EClass theEClass, EObject theEObject)
+  {
+    if (theEClass.eContainer() == modelPackage)
+    {
+      return doSwitch(theEClass.getClassifierID(), theEObject);
+    }
+    else
+    {
+      List<EClass> eSuperTypes = theEClass.getESuperTypes();
+      return
+        eSuperTypes.isEmpty() ?
+          defaultCase(theEObject) :
+          doSwitch(eSuperTypes.get(0), theEObject);
+    }
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
+  protected T doSwitch(int classifierID, EObject theEObject)
+  {
+    switch (classifierID)
+    {
+      case OseeTypesPackage.OSEE_TYPE_MODEL:
+      {
+        OseeTypeModel oseeTypeModel = (OseeTypeModel)theEObject;
+        T result = caseOseeTypeModel(oseeTypeModel);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.IMPORT:
+      {
+        Import import_ = (Import)theEObject;
+        T result = caseImport(import_);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.OSEE_ELEMENT:
+      {
+        OseeElement oseeElement = (OseeElement)theEObject;
+        T result = caseOseeElement(oseeElement);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.OSEE_TYPE:
+      {
+        OseeType oseeType = (OseeType)theEObject;
+        T result = caseOseeType(oseeType);
+        if (result == null) result = caseOseeElement(oseeType);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.ARTIFACT_TYPE:
+      {
+        ArtifactType artifactType = (ArtifactType)theEObject;
+        T result = caseArtifactType(artifactType);
+        if (result == null) result = caseOseeType(artifactType);
+        if (result == null) result = caseOseeElement(artifactType);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.ATTRIBUTE_TYPE_REF:
+      {
+        AttributeTypeRef attributeTypeRef = (AttributeTypeRef)theEObject;
+        T result = caseAttributeTypeRef(attributeTypeRef);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.ATTRIBUTE_TYPE:
+      {
+        AttributeType attributeType = (AttributeType)theEObject;
+        T result = caseAttributeType(attributeType);
+        if (result == null) result = caseOseeType(attributeType);
+        if (result == null) result = caseOseeElement(attributeType);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.OSEE_ENUM_TYPE:
+      {
+        OseeEnumType oseeEnumType = (OseeEnumType)theEObject;
+        T result = caseOseeEnumType(oseeEnumType);
+        if (result == null) result = caseOseeType(oseeEnumType);
+        if (result == null) result = caseOseeElement(oseeEnumType);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.OSEE_ENUM_ENTRY:
+      {
+        OseeEnumEntry oseeEnumEntry = (OseeEnumEntry)theEObject;
+        T result = caseOseeEnumEntry(oseeEnumEntry);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.OSEE_ENUM_OVERRIDE:
+      {
+        OseeEnumOverride oseeEnumOverride = (OseeEnumOverride)theEObject;
+        T result = caseOseeEnumOverride(oseeEnumOverride);
+        if (result == null) result = caseOseeElement(oseeEnumOverride);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.OVERRIDE_OPTION:
+      {
+        OverrideOption overrideOption = (OverrideOption)theEObject;
+        T result = caseOverrideOption(overrideOption);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.ADD_ENUM:
+      {
+        AddEnum addEnum = (AddEnum)theEObject;
+        T result = caseAddEnum(addEnum);
+        if (result == null) result = caseOverrideOption(addEnum);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.REMOVE_ENUM:
+      {
+        RemoveEnum removeEnum = (RemoveEnum)theEObject;
+        T result = caseRemoveEnum(removeEnum);
+        if (result == null) result = caseOverrideOption(removeEnum);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case OseeTypesPackage.RELATION_TYPE:
+      {
+        RelationType relationType = (RelationType)theEObject;
+        T result = caseRelationType(relationType);
+        if (result == null) result = caseOseeType(relationType);
+        if (result == null) result = caseOseeElement(relationType);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      default: return defaultCase(theEObject);
+    }
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Osee Type Model</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Osee Type Model</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOseeTypeModel(OseeTypeModel object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Import</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Import</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseImport(Import object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Osee Element</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Osee Element</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOseeElement(OseeElement object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Osee Type</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Osee Type</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOseeType(OseeType object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Artifact Type</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Artifact Type</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseArtifactType(ArtifactType object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Attribute Type Ref</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Attribute Type Ref</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseAttributeTypeRef(AttributeTypeRef object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Attribute Type</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Attribute Type</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseAttributeType(AttributeType object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Osee Enum Type</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Osee Enum Type</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOseeEnumType(OseeEnumType object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Osee Enum Entry</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Osee Enum Entry</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOseeEnumEntry(OseeEnumEntry object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Osee Enum Override</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Osee Enum Override</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOseeEnumOverride(OseeEnumOverride object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Override Option</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Override Option</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseOverrideOption(OverrideOption object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Add Enum</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Add Enum</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseAddEnum(AddEnum object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Remove Enum</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Remove Enum</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseRemoveEnum(RemoveEnum object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Relation Type</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Relation Type</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseRelationType(RelationType object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>EObject</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch, but this is the last case anyway.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>EObject</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject)
+   * @generated
+   */
+  public T defaultCase(EObject object)
+  {
+    return null;
+  }
+
+} //OseeTypesSwitch
