a8edb4695d3582289613b9738e9997b5cc9902e3
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-04-05 17:36:53 +0000

    "Team Workflow" - UR8MZ - "Word Change Report Showing Changes Incorrectly"

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/commandHandlers/change/ViewWordChangeReportHandler.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/commandHandlers/change/ViewWordChangeReportHandler.java
@@ -8,31 +8,31 @@
  * Contributors:
  *     Boeing - initial API and implementation
  *******************************************************************************/
-package org.eclipse.osee.framework.ui.skynet.commandHandlers;
+package org.eclipse.osee.framework.ui.skynet.commandHandlers.change;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.osee.framework.core.enums.ModificationType;
 import org.eclipse.osee.framework.core.enums.PermissionEnum;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.framework.jdk.core.type.Pair;
 import org.eclipse.osee.framework.logging.OseeLevel;
 import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.access.AccessControlManager;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
-import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
 import org.eclipse.osee.framework.skynet.core.change.Change;
-import org.eclipse.osee.framework.skynet.core.change.RelationChange;
+import org.eclipse.osee.framework.skynet.core.revision.ChangeManager;
 import org.eclipse.osee.framework.ui.plugin.util.AWorkbench;
 import org.eclipse.osee.framework.ui.skynet.SkynetGuiPlugin;
 import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
+import org.eclipse.osee.framework.ui.skynet.commandHandlers.Handlers;
 import org.eclipse.osee.framework.ui.skynet.render.PresentationType;
 import org.eclipse.osee.framework.ui.skynet.render.WordTemplateRenderer;
 import org.eclipse.ui.PlatformUI;
@@ -42,7 +42,8 @@ import org.eclipse.ui.PlatformUI;
  * @author Jeff C. Phillips
  */
 public class ViewWordChangeReportHandler extends AbstractHandler {
-   private List<Change> changes;
+
+   private Collection<Change> changes;
 
    @Override
    public Object execute(ExecutionEvent event) {
@@ -51,61 +52,17 @@ public class ViewWordChangeReportHandler extends AbstractHandler {
    }
 
    public void viewWordChangeReport(Collection<Change> changes, boolean suppressWord, String diffReportFolderName) {
-      ArrayList<Artifact> baseArtifacts = new ArrayList<Artifact>(changes.size());
-      ArrayList<Artifact> newerArtifacts = new ArrayList<Artifact>(changes.size());
       VariableMap variableMap = new VariableMap();
-      String fileName = null;
-
       variableMap.setValue("suppressWord", suppressWord);
       variableMap.setValue("diffReportFolderName", diffReportFolderName);
 
-      for (Change change : changes) {
-         try {
-            Artifact baseArtifact =
-                  (change.getModificationType() == ModificationType.NEW || change.getModificationType() == ModificationType.INTRODUCED) ? null : ArtifactQuery.getHistoricalArtifactFromId(
-                        change.getArtifact().getArtId(), change.getFromTransactionId(), true);
-
-            //Relation changes just pick artifact A and that might not correspond to the correct change modification type.
-            ModificationType newerArtifactModType = change instanceof RelationChange? change.getArtifact().getModType() : change.getModificationType();
-            
-            Artifact newerArtifact =
-               newerArtifactModType.isDeleted() ? null : (change.isHistorical() ? ArtifactQuery.getHistoricalArtifactFromId(
-                        change.getArtifact().getArtId(), change.getToTransactionId(), true) : change.getArtifact());
-
-            baseArtifacts.add(baseArtifact);
-            newerArtifacts.add(newerArtifact);
-
-            if (fileName == null) {
-               if (changes.size() == 1) {
-                  fileName = baseArtifact != null ? baseArtifact.getSafeName() : newerArtifact.getSafeName();
-               } else {
-                  fileName =
-                        baseArtifact != null ? baseArtifact.getBranch().getShortName() : newerArtifact.getBranch().getShortName();
-               }
-               variableMap.setValue("fileName", fileName + "_" + new Date().toString().replaceAll(":", ";") + ".xml");
-            }
-
-         } catch (OseeCoreException ex) {
-            OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE_POPUP, ex);
-         }
-      }
-
-      if (newerArtifacts.size() == 0 || baseArtifacts.size() != newerArtifacts.size()) {
-         throw new IllegalArgumentException(
-               "base artifacts size: " + baseArtifacts.size() + " must match newer artifacts size: " + newerArtifacts.size() + ".");
-      }
-
-      Artifact bArtifact = baseArtifacts.iterator().next();
-      Artifact nArtifact = newerArtifacts.iterator().next();
-
-      Artifact instanceOfArtifact = bArtifact != null ? bArtifact : nArtifact;
+      Collection<Pair<Artifact, Artifact>> compareArtifacts = ChangeManager.getCompareArtifacts(changes);
 
       //All other artifacts types can be rendered by the wordRenderer so the are displayed in the word change report.
       WordTemplateRenderer renderer = new WordTemplateRenderer();
       try {
          renderer.setOptions(variableMap);
-         renderer.compareArtifacts(baseArtifacts, newerArtifacts, new NullProgressMonitor(),
-               instanceOfArtifact.getBranch(), PresentationType.DIFF);
+         renderer.getComparator().compareArtifacts(new NullProgressMonitor(), PresentationType.DIFF, compareArtifacts);
       } catch (OseeCoreException ex) {
          OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE_POPUP, ex);
       }
@@ -117,7 +74,6 @@ public class ViewWordChangeReportHandler extends AbstractHandler {
          return false;
       }
 
-      List<Artifact> artifacts = new LinkedList<Artifact>();
       boolean isEnabled = false;
 
       try {
@@ -128,8 +84,10 @@ public class ViewWordChangeReportHandler extends AbstractHandler {
             IStructuredSelection structuredSelection = (IStructuredSelection) selectionProvider.getSelection();
 
             List<Change> localChanges = Handlers.getArtifactChangesFromStructuredSelection(structuredSelection);
+
             changes = new ArrayList<Change>(localChanges.size());
 
+            Set<Artifact> artifacts = new HashSet<Artifact>();
             for (Change change : localChanges) {
                if (!artifacts.contains(change.getArtifact())) {
                   artifacts.add(change.getArtifact());
