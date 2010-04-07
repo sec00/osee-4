08eeded30c741737e1ac0c2b176f763d70311390
jphillips <jphillips@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-04-07 20:45:45 +0000

    Cleaned up Change Report issues

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/revision/ChangeManager.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/revision/ChangeManager.java
@@ -14,10 +14,10 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.osee.framework.core.client.ClientSessionManager;
 import org.eclipse.osee.framework.core.data.IOseeBranch;
-import org.eclipse.osee.framework.core.enums.ModificationType;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.model.Branch;
 import org.eclipse.osee.framework.core.model.TransactionRecord;
@@ -29,204 +29,200 @@ import org.eclipse.osee.framework.jdk.core.type.CompositeKeyHashMap;
 import org.eclipse.osee.framework.jdk.core.type.HashCollection;
 import org.eclipse.osee.framework.jdk.core.type.Pair;
 import org.eclipse.osee.framework.jdk.core.util.time.GlobalTime;
-import org.eclipse.osee.framework.logging.OseeLevel;
-import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.ArtifactLoader;
 import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
-import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
-import org.eclipse.osee.framework.skynet.core.change.AttributeChange;
 import org.eclipse.osee.framework.skynet.core.change.Change;
-import org.eclipse.osee.framework.skynet.core.change.RelationChange;
-import org.eclipse.osee.framework.skynet.core.internal.Activator;
 import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
 
 /**
  * Public API class for access to change data from branches and transactionIds
- *
+ * 
  * @author Jeff C. Phillips
  * @author Donald G. Dunne
  */
 public final class ChangeManager {
 
-   private final static RevisionChangeLoader revsionChangeLoader = new RevisionChangeLoader();
+	private final static RevisionChangeLoader revsionChangeLoader = new RevisionChangeLoader();
 
-   private ChangeManager() {
-   }
+	private ChangeManager() {
+	}
 
-   public static Collection<Pair<Artifact, Artifact>> getCompareArtifacts(Collection<Change> changes) {
-      Collection<Pair<Artifact, Artifact>> toReturn = new ArrayList<Pair<Artifact, Artifact>>(changes.size());
-      for (Change change : changes) {
-         try {
-            ModificationType modType = change.getModificationType();
-            // REMOVE THIS IF IT DOESN'T WORK
-            if (change instanceof AttributeChange) {
-               modType = change.getArtifact().getModType();
-            }
+	public static Collection<Pair<Artifact, Artifact>> getCompareArtifacts(
+			Collection<Change> changes) {
+		Collection<Pair<Artifact, Artifact>> toReturn = new ArrayList<Pair<Artifact, Artifact>>(
+				changes.size());
+		for (Change change : changes) {
+			Artifact baseArtifact = change.getFromArtifact();
+			Artifact newerArtifact = change.getToArtifact();
+			toReturn.add(new Pair<Artifact, Artifact>(baseArtifact,
+					newerArtifact));
 
-            Artifact baseArtifact = null;
-            if (modType != ModificationType.NEW && modType != ModificationType.INTRODUCED) {
-               baseArtifact =
-                     ArtifactQuery.getHistoricalArtifactFromId(change.getArtifact().getArtId(),
-                           change.getFromTransactionId(), true);
-            }
+		}
+		return toReturn;
+	}
 
-            //Relation changes just pick artifact A and that might not correspond to the correct change modification type.
-            ModificationType newerArtifactModType = modType;
-            if (change instanceof RelationChange) {
-               newerArtifactModType = change.getArtifact().getModType();
-            }
+	/**
+	 * Acquires changes for a particular artifact
+	 * 
+	 * @param artifact
+	 * @param monitor
+	 * @return changes
+	 * @throws OseeCoreException
+	 */
+	public static Collection<Change> getChangesPerArtifact(Artifact artifact,
+			IProgressMonitor monitor) throws OseeCoreException {
+		return revsionChangeLoader.getChangesPerArtifact(artifact, monitor);
+	}
 
-            Artifact newerArtifact = null;
-            if (!newerArtifactModType.isDeleted()) {
-               if (change.isHistorical()) {
-                  newerArtifact =
-                        ArtifactQuery.getHistoricalArtifactFromId(change.getArtifact().getArtId(),
-                              change.getToTransactionId(), true);
-               } else {
-                  newerArtifact = change.getArtifact();
-               }
-            }
-            toReturn.add(new Pair<Artifact, Artifact>(baseArtifact, newerArtifact));
+	/**
+	 * Acquires artifact, relation and attribute changes from a source branch
+	 * since its creation.
+	 * 
+	 * @param transactionId
+	 * @param monitor
+	 * @return changes
+	 * @throws OseeCoreException
+	 */
+	public static Collection<Change> getChangesPerTransaction(
+			TransactionRecord transactionId, IProgressMonitor monitor)
+			throws OseeCoreException {
+		return new ChangeDataLoader().getChanges(null, transactionId, monitor);
+	}
 
-         } catch (OseeCoreException ex) {
-            OseeLog.log(Activator.class, OseeLevel.SEVERE_POPUP, ex);
-         }
-      }
-      return toReturn;
-   }
+	/**
+	 * Acquires artifact, relation and attribute changes from a source branch
+	 * since its creation.
+	 * 
+	 * @param sourceBranch
+	 * @param monitor
+	 * @return changes
+	 * @throws OseeCoreException
+	 */
+	public static Collection<Change> getChangesPerBranch(
+			IOseeBranch sourceBranch, IProgressMonitor monitor)
+			throws OseeCoreException {
+		return new ChangeDataLoader().getChanges(sourceBranch, null, monitor);
+	}
 
-   /**
-    * Acquires changes for a particular artifact
-    *
-    * @param artifact
-    * @param monitor
-    * @return changes
-    * @throws OseeCoreException
-    */
-   public static Collection<Change> getChangesPerArtifact(Artifact artifact, IProgressMonitor monitor) throws OseeCoreException {
-      return revsionChangeLoader.getChangesPerArtifact(artifact, monitor);
-   }
+	/**
+	 * For the given list of artifacts determine which transactions (on that
+	 * artifact's branch) affected that artifact. The branch's baseline
+	 * transaction is excluded.
+	 * 
+	 * @param artifacts
+	 * @return a map of artifact to collection of TransactionIds which affected
+	 *         the given artifact
+	 * @throws OseeCoreException
+	 */
+	public static HashCollection<Artifact, TransactionRecord> getModifingTransactions(
+			Collection<Artifact> artifacts) throws OseeCoreException {
+		List<Object[]> insertParameters = new ArrayList<Object[]>(artifacts
+				.size() * 5);
 
-   /**
-    * Acquires artifact, relation and attribute changes from a source branch since its creation.
-    *
-    * @param transactionId
-    * @param monitor
-    * @return changes
-    * @throws OseeCoreException
-    */
-   public static Collection<Change> getChangesPerTransaction(TransactionRecord transactionId, IProgressMonitor monitor) throws OseeCoreException {
-      return new ChangeDataLoader().getChanges(null, transactionId, monitor);
-   }
+		int queryId = ArtifactLoader.getNewQueryId();
+		Timestamp insertTime = GlobalTime.GreenwichMeanTimestamp();
 
-   /**
-    * Acquires artifact, relation and attribute changes from a source branch since its creation.
-    *
-    * @param sourceBranch
-    * @param monitor
-    * @return changes
-    * @throws OseeCoreException
-    */
-   public static Collection<Change> getChangesPerBranch(IOseeBranch sourceBranch, IProgressMonitor monitor) throws OseeCoreException {
-      return new ChangeDataLoader().getChanges(sourceBranch, null, monitor);
-   }
+		CompositeKeyHashMap<Integer, Branch, Artifact> artifactMap = new CompositeKeyHashMap<Integer, Branch, Artifact>();
+		for (Artifact artifact : artifacts) {
+			Branch branch = artifact.getBranch();
+			artifactMap.put(artifact.getArtId(), branch, artifact);
+			int transactionNumber = TransactionManager.getHeadTransaction(
+					branch).getId();
+			insertParameters.add(new Object[] { queryId, insertTime,
+					artifact.getArtId(), branch.getId(), transactionNumber });
 
-   /**
-    * For the given list of artifacts determine which transactions (on that artifact's branch) affected that artifact.
-    * The branch's baseline transaction is excluded.
-    *
-    * @param artifacts
-    * @return a map of artifact to collection of TransactionIds which affected the given artifact
-    * @throws OseeCoreException
-    */
-   public static HashCollection<Artifact, TransactionRecord> getModifingTransactions(Collection<Artifact> artifacts) throws OseeCoreException {
-      List<Object[]> insertParameters = new ArrayList<Object[]>(artifacts.size() * 5);
+			// for each combination of artifact and its branch hierarchy
+			while (branch.hasParentBranch()) {
+				transactionNumber = branch.getSourceTransaction().getId();
+				branch = branch.getParentBranch();
+				insertParameters
+						.add(new Object[] { queryId, insertTime,
+								artifact.getArtId(), branch.getId(),
+								transactionNumber });
+			}
+		}
 
-      int queryId = ArtifactLoader.getNewQueryId();
-      Timestamp insertTime = GlobalTime.GreenwichMeanTimestamp();
+		HashCollection<Artifact, TransactionRecord> transactionMap = new HashCollection<Artifact, TransactionRecord>();
+		try {
+			ArtifactLoader.insertIntoArtifactJoin(insertParameters);
+			IOseeStatement chStmt = ConnectionHandler.getStatement();
+			try {
+				chStmt.runPreparedQuery(insertParameters.size() * 2,
+						ClientSessionManager
+								.getSql(OseeSql.CHANGE_TX_MODIFYING), queryId);
+				while (chStmt.next()) {
+					Branch branch = BranchManager.getBranch(chStmt
+							.getInt("branch_id"));
+					Artifact artifact = artifactMap.get(
+							chStmt.getInt("art_id"), branch);
+					transactionMap.put(artifact, TransactionManager
+							.getTransactionId(chStmt.getInt("transaction_id")));
+				}
+			} finally {
+				chStmt.close();
+			}
+		} finally {
+			ArtifactLoader.clearQuery(queryId);
+		}
 
-      CompositeKeyHashMap<Integer, Branch, Artifact> artifactMap = new CompositeKeyHashMap<Integer, Branch, Artifact>();
-      for (Artifact artifact : artifacts) {
-         Branch branch = artifact.getBranch();
-         artifactMap.put(artifact.getArtId(), branch, artifact);
-         int transactionNumber = TransactionManager.getHeadTransaction(branch).getId();
-         insertParameters.add(new Object[] {queryId, insertTime, artifact.getArtId(), branch.getId(), transactionNumber});
+		return transactionMap;
+	}
 
-         // for each combination of artifact and its branch hierarchy
-         while (branch.hasParentBranch()) {
-            transactionNumber = branch.getSourceTransaction().getId();
-            branch = branch.getParentBranch();
-            insertParameters.add(new Object[] {queryId, insertTime, artifact.getArtId(), branch.getId(),
-                  transactionNumber});
-         }
-      }
+	/**
+	 * For the given list of artifacts determine which branches (in the branch
+	 * hierarchy for that artifact) affected that artifact.
+	 * 
+	 * @param artifacts
+	 * @return a map of artifact to collection of branches which affected the
+	 *         given artifact
+	 * @throws OseeCoreException
+	 */
+	public static HashCollection<Artifact, Branch> getModifingBranches(
+			Collection<Artifact> artifacts) throws OseeCoreException {
+		List<Object[]> insertParameters = new ArrayList<Object[]>(artifacts
+				.size() * 5);
+		int queryId = ArtifactLoader.getNewQueryId();
+		Timestamp insertTime = GlobalTime.GreenwichMeanTimestamp();
 
-      HashCollection<Artifact, TransactionRecord> transactionMap = new HashCollection<Artifact, TransactionRecord>();
-      try {
-         ArtifactLoader.insertIntoArtifactJoin(insertParameters);
-         IOseeStatement chStmt = ConnectionHandler.getStatement();
-         try {
-            chStmt.runPreparedQuery(insertParameters.size() * 2,
-                  ClientSessionManager.getSql(OseeSql.CHANGE_TX_MODIFYING), queryId);
-            while (chStmt.next()) {
-               Branch branch = BranchManager.getBranch(chStmt.getInt("branch_id"));
-               Artifact artifact = artifactMap.get(chStmt.getInt("art_id"), branch);
-               transactionMap.put(artifact, TransactionManager.getTransactionId(chStmt.getInt("transaction_id")));
-            }
-         } finally {
-            chStmt.close();
-         }
-      } finally {
-         ArtifactLoader.clearQuery(queryId);
-      }
+		CompositeKeyHashMap<Integer, Branch, Artifact> artifactMap = new CompositeKeyHashMap<Integer, Branch, Artifact>();
+		for (Artifact artifact : artifacts) {
+			artifactMap
+					.put(artifact.getArtId(), artifact.getBranch(), artifact);
+			// for each combination of artifact and all working branches in its
+			// hierarchy
+			for (Branch workingBranch : BranchManager
+					.getWorkingBranches(artifact.getBranch())) {
+				insertParameters.add(new Object[] { queryId, insertTime,
+						artifact.getArtId(), workingBranch.getId(),
+						SQL3DataType.INTEGER });
+			}
+		}
 
-      return transactionMap;
-   }
-
-   /**
-    * For the given list of artifacts determine which branches (in the branch hierarchy for that artifact) affected that
-    * artifact.
-    *
-    * @param artifacts
-    * @return a map of artifact to collection of branches which affected the given artifact
-    * @throws OseeCoreException
-    */
-   public static HashCollection<Artifact, Branch> getModifingBranches(Collection<Artifact> artifacts) throws OseeCoreException {
-      List<Object[]> insertParameters = new ArrayList<Object[]>(artifacts.size() * 5);
-      int queryId = ArtifactLoader.getNewQueryId();
-      Timestamp insertTime = GlobalTime.GreenwichMeanTimestamp();
-
-      CompositeKeyHashMap<Integer, Branch, Artifact> artifactMap = new CompositeKeyHashMap<Integer, Branch, Artifact>();
-      for (Artifact artifact : artifacts) {
-         artifactMap.put(artifact.getArtId(), artifact.getBranch(), artifact);
-         // for each combination of artifact and all working branches in its hierarchy
-         for (Branch workingBranch : BranchManager.getWorkingBranches(artifact.getBranch())) {
-            insertParameters.add(new Object[] {queryId, insertTime, artifact.getArtId(), workingBranch.getId(),
-                  SQL3DataType.INTEGER});
-         }
-      }
-
-      HashCollection<Artifact, Branch> branchMap = new HashCollection<Artifact, Branch>();
-      try {
-         ArtifactLoader.insertIntoArtifactJoin(insertParameters);
-         IOseeStatement chStmt = ConnectionHandler.getStatement();
-         try {
-            chStmt.runPreparedQuery(insertParameters.size() * 2,
-                  ClientSessionManager.getSql(OseeSql.CHANGE_BRANCH_MODIFYING), queryId);
-            while (chStmt.next()) {
-               if (chStmt.getInt("tx_count") > 0) {
-                  Branch branch = BranchManager.getBranch(chStmt.getInt("branch_id"));
-                  Artifact artifact = artifactMap.get(chStmt.getInt("art_id"), branch.getParentBranch());
-                  branchMap.put(artifact, branch);
-               }
-            }
-         } finally {
-            chStmt.close();
-         }
-      } finally {
-         ArtifactLoader.clearQuery(queryId);
-      }
-      return branchMap;
-   }
+		HashCollection<Artifact, Branch> branchMap = new HashCollection<Artifact, Branch>();
+		try {
+			ArtifactLoader.insertIntoArtifactJoin(insertParameters);
+			IOseeStatement chStmt = ConnectionHandler.getStatement();
+			try {
+				chStmt.runPreparedQuery(insertParameters.size() * 2,
+						ClientSessionManager
+								.getSql(OseeSql.CHANGE_BRANCH_MODIFYING),
+						queryId);
+				while (chStmt.next()) {
+					if (chStmt.getInt("tx_count") > 0) {
+						Branch branch = BranchManager.getBranch(chStmt
+								.getInt("branch_id"));
+						Artifact artifact = artifactMap.get(chStmt
+								.getInt("art_id"), branch.getParentBranch());
+						branchMap.put(artifact, branch);
+					}
+				}
+			} finally {
+				chStmt.close();
+			}
+		} finally {
+			ArtifactLoader.clearQuery(queryId);
+		}
+		return branchMap;
+	}
 }
