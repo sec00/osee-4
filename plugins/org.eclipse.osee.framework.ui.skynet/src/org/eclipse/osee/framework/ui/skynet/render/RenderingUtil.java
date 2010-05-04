69dfae244a64405c675704e598e66e5adabdf047
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-04 00:22:26 +0000

    fix inversion of start and end versions for diff

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/RenderingUtil.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/RenderingUtil.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
@@ -28,149 +29,165 @@ import org.eclipse.osee.framework.skynet.core.word.WordAnnotationHandler;
 import org.eclipse.osee.framework.ui.skynet.preferences.MsWordPreferencePage;
 
 public final class RenderingUtil {
-   private static final Random generator = new Random();
+	private static final Random generator = new Random();
 
-   private static IFolder workingFolder;
-   private static IFolder diffFolder;
-   private static IFolder previewFolder;
-   private static IFolder mergeEditFolder;
-   private static boolean arePopupsAllowed = true;
+	private static IFolder workingFolder;
+	private static IFolder diffFolder;
+	private static IFolder previewFolder;
+	private static IFolder mergeEditFolder;
+	private static boolean arePopupsAllowed = true;
 
-   private RenderingUtil() {
-   }
+	private RenderingUtil() {
+	}
 
-   public static void setPopupsAllowed(boolean popupsAllowed) {
-      arePopupsAllowed = popupsAllowed;
-   }
+	public static void setPopupsAllowed(boolean popupsAllowed) {
+		arePopupsAllowed = popupsAllowed;
+	}
 
-   public static boolean arePopupsAllowed() {
-      return arePopupsAllowed;
-   }
+	public static boolean arePopupsAllowed() {
+		return arePopupsAllowed;
+	}
 
-   public static Pair<Artifact, Artifact> asRenderInput(ArtifactDelta delta) {
-      Artifact artFile1;
-      Artifact artFile2;
+	public static Pair<Artifact, Artifact> asRenderInput(ArtifactDelta delta) {
+		Artifact artFile1;
+		Artifact artFile2;
 
-      if (delta.getTxDelta() == null || !delta.getTxDelta().areOnTheSameBranch()) {
-         // Assumptions - when comparing data between transactions on different branches, the start artifact will never be null;
-         ModificationType startModType = delta.getStartArtifact().getModType();
-         if (startModType.isDeleted()) {
-            artFile1 = delta.getStartArtifact();
-            artFile2 = null;
-         } else if ((startModType == ModificationType.INTRODUCED || startModType == ModificationType.NEW) && delta.getEndArtifact() == null) {
-            artFile1 = null;
-            artFile2 = delta.getStartArtifact();
-         } else {
-            artFile1 = delta.getStartArtifact();
-            artFile2 = delta.getEndArtifact();
-         }
-      } else {
-         // Assumptions - when comparing data between transactions on the same branch, the end artifact will never be null;
-         ModificationType modType = delta.getEndArtifact().getModType();
-         if (modType.isDeleted()) {
-            artFile1 = delta.getStartArtifact();
-            artFile2 = null;
-         } else if ((modType == ModificationType.INTRODUCED || modType == ModificationType.NEW) && delta.getStartArtifact() == null) {
-            artFile1 = null;
-            artFile2 = delta.getEndArtifact();
-         } else {
-            artFile1 = delta.getEndArtifact();
-            artFile2 = delta.getStartArtifact();
-         }
-      }
-      return new Pair<Artifact, Artifact>(artFile1, artFile2);
-   }
+		if (delta.getTxDelta() == null
+				|| !delta.getTxDelta().areOnTheSameBranch()) {
+			// Assumptions - when comparing data between transactions on
+			// different branches, the start artifact will never be null;
+			ModificationType startModType = delta.getStartArtifact()
+					.getModType();
+			if (startModType.isDeleted()) {
+				artFile1 = delta.getStartArtifact();
+				artFile2 = null;
+			} else if ((startModType == ModificationType.INTRODUCED || startModType == ModificationType.NEW)
+					&& delta.getEndArtifact() == null) {
+				artFile1 = null;
+				artFile2 = delta.getStartArtifact();
+			} else {
+				artFile1 = delta.getStartArtifact();
+				artFile2 = delta.getEndArtifact();
+			}
+		} else {
+			// Assumptions - when comparing data between transactions on the
+			// same branch, the end artifact will never be null;
+			ModificationType modType = delta.getEndArtifact().getModType();
+			if (modType.isDeleted()) {
+				artFile1 = delta.getStartArtifact();
+				artFile2 = null;
+			} else if ((modType == ModificationType.INTRODUCED || modType == ModificationType.NEW)
+					&& delta.getStartArtifact() == null) {
+				artFile1 = null;
+				artFile2 = delta.getEndArtifact();
+			} else {
+				artFile1 = delta.getStartArtifact();
+				artFile2 = delta.getEndArtifact();
+			}
+		}
+		return new Pair<Artifact, Artifact>(artFile1, artFile2);
+	}
 
-   public static String getFilenameFromArtifact(FileSystemRenderer renderer, Artifact artifact, PresentationType presentationType) throws OseeCoreException {
-      StringBuilder name = new StringBuilder(100);
+	public static String getFilenameFromArtifact(FileSystemRenderer renderer,
+			Artifact artifact, PresentationType presentationType)
+			throws OseeCoreException {
+		StringBuilder name = new StringBuilder(100);
 
-      if (artifact != null) {
-         name.append(artifact.getSafeName());
-         name.append("(");
-         name.append(artifact.getGuid());
-         name.append(")");
+		if (artifact != null) {
+			name.append(artifact.getSafeName());
+			name.append("(");
+			name.append(artifact.getGuid());
+			name.append(")");
 
-         if (artifact.isHistorical() || presentationType == PresentationType.DIFF) {
-            name.append("(");
-            name.append(artifact.getTransactionNumber());
-            name.append(")");
-         }
+			if (artifact.isHistorical()
+					|| presentationType == PresentationType.DIFF) {
+				name.append("(");
+				name.append(artifact.getTransactionNumber());
+				name.append(")");
+			}
 
-         name.append(" ");
-         name.append(new Date().toString().replaceAll(":", ";"));
-         name.append("-");
-         name.append(generator.nextInt(99) + 1);
-         name.append(".");
-         name.append(renderer.getAssociatedExtension(artifact));
-      } else {
-         name.append(GUID.create());
-         name.append(".xml");
-      }
-      return name.toString();
-   }
+			name.append(" ");
+			name.append(new Date().toString().replaceAll(":", ";"));
+			name.append("-");
+			name.append(generator.nextInt(99) + 1);
+			name.append(".");
+			name.append(renderer.getAssociatedExtension(artifact));
+		} else {
+			name.append(GUID.create());
+			name.append(".xml");
+		}
+		return name.toString();
+	}
 
-   public static IFolder getRenderFolder(Branch branch, PresentationType presentationType) throws OseeCoreException {
-      try {
-         IFolder baseFolder = ensureRenderFolderExists(presentationType);
-         IFolder renderFolder = baseFolder.getFolder(BranchManager.toFileName(branch));
-         if (!renderFolder.exists()) {
-            renderFolder.create(true, true, null);
-         }
-         return renderFolder;
-      } catch (CoreException ex) {
-         throw new OseeCoreException(ex);
-      }
-   }
+	public static IFolder getRenderFolder(Branch branch,
+			PresentationType presentationType) throws OseeCoreException {
+		try {
+			IFolder baseFolder = ensureRenderFolderExists(presentationType);
+			IFolder renderFolder = baseFolder.getFolder(BranchManager
+					.toFileName(branch));
+			if (!renderFolder.exists()) {
+				renderFolder.create(true, true, null);
+			}
+			return renderFolder;
+		} catch (CoreException ex) {
+			throw new OseeCoreException(ex);
+		}
+	}
 
-   public static IFolder ensureRenderFolderExists(PresentationType presentationType) throws OseeCoreException {
-      IFolder toReturn = null;
-      switch (presentationType) {
-         case DIFF:
-            diffFolder = getOrCreateFolder(diffFolder, ".diff");
-            toReturn = diffFolder;
-            break;
-         case SPECIALIZED_EDIT:
-            workingFolder = getOrCreateFolder(workingFolder, ".working");
-            toReturn = workingFolder;
-            break;
-         case PREVIEW:
-            previewFolder = getOrCreateFolder(previewFolder, ".preview");
-            toReturn = previewFolder;
-            break;
-         case MERGE_EDIT:
-            mergeEditFolder = getOrCreateFolder(mergeEditFolder, ".mergeEdit");
-            toReturn = mergeEditFolder;
-            break;
-         default:
-            throw new OseeArgumentException("Unexpected presentation type");
-      }
-      return toReturn;
-   }
+	public static IFolder ensureRenderFolderExists(
+			PresentationType presentationType) throws OseeCoreException {
+		IFolder toReturn = null;
+		switch (presentationType) {
+		case DIFF:
+			diffFolder = getOrCreateFolder(diffFolder, ".diff");
+			toReturn = diffFolder;
+			break;
+		case SPECIALIZED_EDIT:
+			workingFolder = getOrCreateFolder(workingFolder, ".working");
+			toReturn = workingFolder;
+			break;
+		case PREVIEW:
+			previewFolder = getOrCreateFolder(previewFolder, ".preview");
+			toReturn = previewFolder;
+			break;
+		case MERGE_EDIT:
+			mergeEditFolder = getOrCreateFolder(mergeEditFolder, ".mergeEdit");
+			toReturn = mergeEditFolder;
+			break;
+		default:
+			throw new OseeArgumentException("Unexpected presentation type");
+		}
+		return toReturn;
+	}
 
-   private static IFolder getOrCreateFolder(IFolder folder, String name) throws OseeCoreException {
-      IFolder toCheck = folder;
-      if (toCheck == null || !toCheck.exists()) {
-         toCheck = OseeData.getFolder(name);
-      }
-      return toCheck;
-   }
+	private static IFolder getOrCreateFolder(IFolder folder, String name)
+			throws OseeCoreException {
+		IFolder toCheck = folder;
+		if (toCheck == null || !toCheck.exists()) {
+			toCheck = OseeData.getFolder(name);
+		}
+		return toCheck;
+	}
 
-   public static Set<Artifact> checkForTrackedChangesOn(Artifact artifact) throws OseeCoreException {
-      Set<Artifact> artifacts = new HashSet<Artifact>();
+	public static Set<Artifact> checkForTrackedChangesOn(Artifact artifact)
+			throws OseeCoreException {
+		Set<Artifact> artifacts = new HashSet<Artifact>();
 
-      if (!UserManager.getUser().getBooleanSetting(MsWordPreferencePage.REMOVE_TRACKED_CHANGES)) {
-         if (artifact != null) {
-            Attribute<?> attribute = artifact.getSoleAttribute(CoreAttributeTypes.WORD_TEMPLATE_CONTENT);
-            if (attribute != null) {
-               String value = attribute.getValue().toString();
-               // check for track changes
-               if (WordAnnotationHandler.containsWordAnnotations(value)) {
-                  // capture those artifacts that have tracked changes on
-                  artifacts.add(artifact);
-               }
-            }
-         }
-      }
-      return artifacts;
-   }
+		if (!UserManager.getUser().getBooleanSetting(
+				MsWordPreferencePage.REMOVE_TRACKED_CHANGES)) {
+			if (artifact != null) {
+				Attribute<?> attribute = artifact
+						.getSoleAttribute(CoreAttributeTypes.WORD_TEMPLATE_CONTENT);
+				if (attribute != null) {
+					String value = attribute.getValue().toString();
+					// check for track changes
+					if (WordAnnotationHandler.containsWordAnnotations(value)) {
+						// capture those artifacts that have tracked changes on
+						artifacts.add(artifact);
+					}
+				}
+			}
+		}
+		return artifacts;
+	}
 }
