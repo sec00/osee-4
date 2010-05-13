9000293e7bfc86bed51eaa7cc9a4e641e887bc86
rschmitt <rschmitt@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-13 18:34:52 +0000

    Tolerate invalid references to other artifacts when rendering relation order in Word

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/ArtifactGuidToWordML.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/ArtifactGuidToWordML.java
@@ -12,6 +12,7 @@ package org.eclipse.osee.framework.ui.skynet.render;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.eclipse.osee.framework.core.exception.ArtifactDoesNotExist;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.model.Branch;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
@@ -33,8 +34,12 @@ public class ArtifactGuidToWordML {
    public List<String> resolveAsOseeLinks(Branch branch, List<String> artifactGuids) throws OseeCoreException {
       List<String> mlLinks = new ArrayList<String>();
       for (String guid : artifactGuids) {
-         Artifact artifact = ArtifactQuery.getArtifactFromId(guid, branch);
-         mlLinks.add(linkBuilder.getWordMlLink(LinkType.OSEE_SERVER_LINK, artifact));
+         try {
+            Artifact artifact = ArtifactQuery.getArtifactFromId(guid, branch);
+            mlLinks.add(linkBuilder.getWordMlLink(LinkType.OSEE_SERVER_LINK, artifact));
+         } catch (ArtifactDoesNotExist e) {
+            break;
+         }
       }
       return mlLinks;
    }
