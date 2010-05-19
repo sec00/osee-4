b1754187cf1e70cf5bd8c9c9c922cef6ea59e5cd
rschmitt <rschmitt@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-19 17:14:04 +0000

    Bug 311152 -  Cannot commit a branch more than once

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/revision/ChangeManager.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/revision/ChangeManager.java
@@ -70,8 +70,7 @@ public final class ChangeManager {
    }
 
    /**
-    * Acquires artifact, relation and attribute changes from a source branch
-    * since its creation.
+    * Acquires artifact, relation and attribute changes from a source branch since its creation.
     * 
     * @param transactionId
     * @param monitor
@@ -87,8 +86,7 @@ public final class ChangeManager {
    }
 
    /**
-    * Acquires artifact, relation and attribute changes from a source branch
-    * since its creation.
+    * Acquires artifact, relation and attribute changes from a source branch since its creation.
     * 
     * @param sourceBranch
     * @param monitor
@@ -105,10 +103,15 @@ public final class ChangeManager {
       return new ChangeDataLoader(changes, txDelta);
    }
 
+   public static IOperation compareTwoBranchesHead(IOseeBranch sourceBranch, IOseeBranch destinationBranch, Collection<Change> changes) throws OseeCoreException {
+      TransactionRecord startTx = TransactionManager.getHeadTransaction(sourceBranch);
+      TransactionRecord endTx = TransactionManager.getHeadTransaction(destinationBranch);
+      return new ChangeDataLoader(changes, new TransactionDelta(startTx, endTx));
+   }
+
    /**
-    * For the given list of artifacts determine which transactions (on that
-    * artifact's branch) affected that artifact. The branch's baseline
-    * transaction is excluded.
+    * For the given list of artifacts determine which transactions (on that artifact's branch) affected that artifact.
+    * The branch's baseline transaction is excluded.
     * 
     * @param artifacts
     * @return a map of artifact to collection of TransactionIds which affected
@@ -160,8 +163,8 @@ public final class ChangeManager {
    }
 
    /**
-    * For the given list of artifacts determine which branches (in the branch
-    * hierarchy for that artifact) affected that artifact.
+    * For the given list of artifacts determine which branches (in the branch hierarchy for that artifact) affected that
+    * artifact.
     * 
     * @param artifacts
     * @return a map of artifact to collection of branches which affected the
