6848fc76336768f29a7bc762c08930c6b8aff104
Morgan E. Cook <Morgan.e.cook@boeing.com>
2016-08-11 13:40:10 -0700

    refactor: Move word template content rendering to server
    Change-Id: I081ffcae1a7a82fde0140107989589d42907fc13
    Signed-off-by: Morgan E. Cook <Morgan.e.cook@boeing.com>

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/WordTemplateRenderer.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/WordTemplateRenderer.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -27,20 +26,23 @@ import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.osee.define.report.api.ReportConstants;
+import org.eclipse.osee.define.report.api.WordTemplateContentData;
+import org.eclipse.osee.framework.core.client.ClientSessionManager;
 import org.eclipse.osee.framework.core.data.BranchId;
 import org.eclipse.osee.framework.core.data.IAttributeType;
+import org.eclipse.osee.framework.core.data.TransactionId;
 import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
 import org.eclipse.osee.framework.core.enums.CoreRelationTypes;
 import org.eclipse.osee.framework.core.operation.IOperation;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
+import org.eclipse.osee.framework.jdk.core.type.Pair;
 import org.eclipse.osee.framework.jdk.core.util.xml.Jaxp;
 import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
-import org.eclipse.osee.framework.skynet.core.artifact.Attribute;
+import org.eclipse.osee.framework.skynet.core.artifact.ArtifactURL;
+import org.eclipse.osee.framework.skynet.core.httpRequests.HttpWordUpdateRequest;
 import org.eclipse.osee.framework.skynet.core.linking.LinkType;
 import org.eclipse.osee.framework.skynet.core.linking.OseeLinkBuilder;
-import org.eclipse.osee.framework.skynet.core.linking.WordMlLinkHandler;
 import org.eclipse.osee.framework.skynet.core.types.IArtifact;
 import org.eclipse.osee.framework.skynet.core.word.WordUtil;
 import org.eclipse.osee.framework.ui.skynet.MenuCmdDef;
@@ -143,51 +145,39 @@ public class WordTemplateRenderer extends WordRenderer implements ITemplateRende
    public void renderAttribute(IAttributeType attributeType, Artifact artifact, PresentationType presentationType, Producer producer, AttributeElement attributeElement, String footer) throws OseeCoreException {
       WordMLProducer wordMl = (WordMLProducer) producer;
 
+      String data = null;
       if (attributeType.equals(CoreAttributeTypes.WordTemplateContent)) {
-         Attribute<?> wordTempConAttr = artifact.getSoleAttribute(attributeType);
-         String data = null;
-         if (wordTempConAttr != null) {
-            data = (String) wordTempConAttr.getValue();
-         }
+
+         LinkType linkType = (LinkType) getOption("linkType");
 
          if (attributeElement.getLabel().length() > 0) {
             wordMl.addParagraph(attributeElement.getLabel());
          }
 
-         if (data != null) {
-            //Change the BinData Id so images do not get overridden by the other images
-            data = WordUtil.reassignBinDataID(data);
+         TransactionId txId = null;
+         if (artifact.isHistorical()) {
+            txId = artifact.getTransaction();
+         } else {
+            txId = TransactionId.SENTINEL;
+         }
 
-            LinkType linkType = (LinkType) getOption("linkType");
-            Set<String> unknownGuids = new HashSet<>();
-            data = WordMlLinkHandler.link(linkType, artifact, data, unknownGuids);
-            WordUiUtil.displayUnknownGuids(artifact, unknownGuids);
+         String oseeLink = ArtifactURL.getOpenInOseeLink(artifact).toString();
 
-            data = WordUtil.reassignBookMarkID(data);
+         WordTemplateContentData wtcData = new WordTemplateContentData();
+         wtcData.setArtId(artifact.getUuid());
+         wtcData.setBranchId(artifact.getBranch());
+         wtcData.setFooter(footer);
+         wtcData.setIsEdit(presentationType == PresentationType.SPECIALIZED_EDIT);
+         wtcData.setLinkType(linkType != null ? linkType.toString() : null);
+         wtcData.setTxId(txId);
+         wtcData.setSessionId(ClientSessionManager.getSessionId());
+         wtcData.setOseeLink(oseeLink);
 
-            // remove any existing footers and replace with the current one
-            // first try to remove footer for extra paragraphs
-            if (presentationType == PresentationType.SPECIALIZED_EDIT) {
-               data = data.replaceAll(ReportConstants.ENTIRE_FTR_EXTRA_PARA, "");
-            }
+         Pair<String, Set<String>> content = HttpWordUpdateRequest.renderWordTemplateContent(wtcData);
 
-            // if no extra paragraphs have been added this will replace the normal footer
-            data = data.replaceAll(ReportConstants.ENTIRE_FTR, "");
-            data = data.replaceAll(ReportConstants.NO_DATA_RIGHTS, "");
-
-            // remove extra paragraph inserted
-            if (presentationType == PresentationType.SPECIALIZED_EDIT && !data.contains("<w:tbl>")) {
-               int lastIndex = data.lastIndexOf("<w:p wsp:rsidR=");
-
-               if (lastIndex != -1) {
-                  // temp should equal <w:p wsp:rsidR ..</w:p> ...
-                  String temp = data.substring(lastIndex);
-                  temp = temp.replaceAll("<w:p wsp:rsidR=\".*?\" wsp:rsidRDefault=\".*?\"></w:p>", "");
-                  data = data.substring(0, lastIndex) + temp;
-               }
-            }
-
-            data = data.concat(footer);
+         if (content != null) {
+            data = content.getFirst();
+            WordUiUtil.displayUnknownGuids(artifact, content.getSecond());
          }
 
          if (presentationType == PresentationType.SPECIALIZED_EDIT) {
@@ -197,7 +187,6 @@ public class WordTemplateRenderer extends WordRenderer implements ITemplateRende
             wordMl.addEditParagraphNoEscape(linkBuilder.getEndEditImage(artifact.getGuid()));
 
          } else if (data != null) {
-            data = data.replaceAll(WordTemplateProcessor.PGNUMTYPE_START_1, "");
             wordMl.addWordMl(data);
          }
          wordMl.resetListValue();
