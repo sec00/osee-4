8d8ddd08a23d29cb930af91480ac7daa6f9c0022
donald.g.dunne <donald.g.dunne@boeing.com>
2016-08-24 15:49:04 -0700

    refinement: Improve AbstractAtsUserService.getCurrent
    Change-Id: I01a5cfb2b25ab4754b22944bd44a2fe1172da462

--- plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/users/AbstractAtsUserService.java
+++ plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/users/AbstractAtsUserService.java
@@ -46,6 +46,9 @@ public abstract class AbstractAtsUserService implements IAtsUserService {
                }
             }
          }
+         if (currentUser == null) {
+            currentUser = loadUserFromDbByUserId(getCurrentUserId());
+         }
       }
       return currentUser;
    }
