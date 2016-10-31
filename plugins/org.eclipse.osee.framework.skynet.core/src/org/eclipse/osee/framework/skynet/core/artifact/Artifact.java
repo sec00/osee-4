25456f5f054eea4e00295cf6f689ea38c9abf609
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2016-10-31 13:18:23 -0700

    refactor: Remove IBasicArtifact.getFullArtifact
    Change-Id: Iba143daabbefb361ec58e085fa0e0185254d163e

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/Artifact.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/Artifact.java
@@ -1688,11 +1688,6 @@ public class Artifact extends FullyNamedIdentity<String> implements IArtifact, A
       return modType;
    }
 
-   @Override
-   public final Artifact getFullArtifact() {
-      return this;
-   }
-
    public final DefaultBasicGuidArtifact getBasicGuidArtifact() {
       return new DefaultBasicGuidArtifact(getBranch(), getArtifactTypeId(), this);
    }
