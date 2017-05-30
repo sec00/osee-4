61873c09ce9284903b6d0cca9d38e6b0c5e25142
Angel Avila <angel.avila2@boeing.com>
2017-05-30 14:07:59 -0700

    feature[ats_ATS405954]: Update Coverage to work with Windown paths
    Change-Id: Ic56ce287e11640c02bb95b1457808431306b624b

--- plugins/org.eclipse.osee.vcast/src/org/eclipse/osee/vcast/VCastLisFileParser.java
+++ plugins/org.eclipse.osee.vcast/src/org/eclipse/osee/vcast/VCastLisFileParser.java
@@ -14,52 +14,33 @@ import java.io.File;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.eclipse.osee.framework.jdk.core.type.OseeArgumentException;
 import org.eclipse.osee.framework.jdk.core.type.Pair;
 import org.eclipse.osee.framework.jdk.core.util.Lib;
-import org.eclipse.osee.vcast.model.ICoverageUnitFileContentsLoader;
 
 /**
  * @author Shawn F. Cook
  */
-public class VCastLisFileParser implements ICoverageUnitFileContentsLoader {
+public class VCastLisFileParser {
 
    private static final Pattern sourceLinePattern = Pattern.compile("^[0-9]+ [0-9]+(.*?)$");
    private static final Pattern exceptionPattern = Pattern.compile("^\\s+EXCEPTION\\s*$");
    private static final Pattern endMethodPattern = Pattern.compile("^\\s*END\\s+(.*);\\s*$");
 
-   private final String lisFileName;
-   private final String vCastDir;
+   private final File file;
 
-   private boolean populated = false;
    private String fileText = null;
    private String[] lisFileLines;
 
-   public VCastLisFileParser(String lisFileName, String vCastDir) {
-      this.lisFileName = lisFileName;
-      this.vCastDir = vCastDir;
+   public VCastLisFileParser(File file) {
+      this.file = file;
    }
 
-   private synchronized void ensurePopulated() {
-      if (!populated) {
-         populated = true;
-         String lisFilePathName = vCastDir + File.separator + lisFileName;
-         File lisFile = new File(lisFilePathName);
-         if (!lisFile.exists()) {
-            throw new OseeArgumentException(String.format("VectorCast *.LIS file doesn't exist [%s]", lisFilePathName));
-         }
-         try {
-            fileText = Lib.fileToString(lisFile);
-         } catch (IOException ex) {
-            throw new OseeArgumentException(String.format("IO Exception trying to read file [%s]", lisFilePathName));
-         }
-         lisFileLines = fileText.split("\n");
-      }
+   public void loadFileText() throws IOException {
+      fileText = Lib.fileToString(file);
+      lisFileLines = fileText.split("\n");
    }
 
    public Pair<String, Boolean> getSourceCodeForLine(Integer method, Integer executionLine) {
-      ensurePopulated();
-
       String startsWith = method + " " + executionLine + " ";
       boolean exceptionLine = false;
       for (String line : lisFileLines) {
@@ -84,11 +65,5 @@ public class VCastLisFileParser implements ICoverageUnitFileContentsLoader {
          }
       }
       return null;
-   }
-
-   @Override
-   public String getText() {
-      ensurePopulated();
-      return fileText;
    }
 }
