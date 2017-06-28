009539d24eda00e4a82328e370e0a1c18c878730
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2017-06-28 15:33:48 -0700

    refactor: Use ArtifactId in ArtifactQuery.getHistoricalArtifactFromId
    Change-Id: I0821e729e039d8271714a2f394a59de419276648

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/transaction/TransactionManager.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/transaction/TransactionManager.java
@@ -290,7 +290,7 @@ public final class TransactionManager {
       }
 
       TransactionId prevTransId = getPreviousTransactionId(art, revertTransaction);
-      Artifact prevArt = ArtifactQuery.getHistoricalArtifactFromId(art.getId().intValue(),
+      Artifact prevArt = ArtifactQuery.getHistoricalArtifactFromId(art,
          TransactionToken.valueOf(prevTransId, art.getBranch()), DeletionFlag.EXCLUDE_DELETED);
 
       boolean changed = false;
