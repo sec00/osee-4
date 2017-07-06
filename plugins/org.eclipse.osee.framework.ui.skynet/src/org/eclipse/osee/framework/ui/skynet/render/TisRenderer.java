3a57de214a43b626c3ea3271202533c5757a383d
Morgan E. Cook <morgan.e.cook@boeing.com>
2017-07-06 12:23:51 -0700

    refactor: Move DataRightsClassification & CommandGroup to enum util.
    Signed-off-by: Morgan E. Cook <morgan.e.cook@boeing.com>
    Change-Id: I768db8623ddf3ac5b9f68569dd870409ef96966d
    Signed-off-by: Morgan E. Cook <morgan.e.cook@boeing.com>

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/TisRenderer.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/TisRenderer.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.osee.framework.core.enums.CommandGroup;
 import org.eclipse.osee.framework.core.enums.CoreArtifactTypes;
 import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
 import org.eclipse.osee.framework.core.enums.PresentationType;
