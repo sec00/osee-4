4f64caa455b0ca2a44b107e93dec168655f57a20
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2017-07-13 12:55:55 -0700

    refactor: Use ArtifactToken in RelationLink.getOtherSideArtifact
    Change-Id: I370ff9ac11457c7f7cfc3692b1117de8476e14b7

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/Artifact.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/Artifact.java
@@ -1654,7 +1654,7 @@ public class Artifact extends NamedIdBase implements IArtifact, Adaptable, Fully
       ArrayList<RelationLink> relations = new ArrayList<>();
       for (RelationLink relation : getRelationsAll(DeletionFlag.EXCLUDE_DELETED)) {
          try {
-            if (relation.getArtifactOnOtherSide(this).equals(artifact)) {
+            if (relation.getOtherSideArtifact(this).equals(artifact)) {
                relations.add(relation);
             }
          } catch (ArtifactDoesNotExist ex) {
@@ -1676,7 +1676,7 @@ public class Artifact extends NamedIdBase implements IArtifact, Adaptable, Fully
       ArrayList<RelationLink> relations = new ArrayList<>();
       for (RelationLink relation : getRelations(side)) {
          try {
-            if (relation.getArtifactOnOtherSide(this).equals(artifact)) {
+            if (relation.getOtherSideArtifact(this).equals(artifact)) {
                relations.add(relation);
             }
          } catch (ArtifactDoesNotExist ex) {
