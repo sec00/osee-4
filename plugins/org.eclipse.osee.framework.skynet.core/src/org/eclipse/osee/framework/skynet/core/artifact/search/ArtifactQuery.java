41a70ef1661bfe41449eb6903dc92b25174577ea
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2017-07-13 16:29:17 -0700

    refactor: Use ArtifactId in RelationManager
    Change-Id: I4b26157a4bf7e1ba6922747fef012adc3c6d28c8

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/search/ArtifactQuery.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/search/ArtifactQuery.java
@@ -129,8 +129,8 @@ public class ArtifactQuery {
       return getOrCheckArtifactFromId(artId, branch, allowDeleted, QueryType.GET);
    }
 
-   public static Artifact getArtifactFromIdOrNull(int artId, BranchId branch, DeletionFlag allowDeleted) throws OseeCoreException {
-      return getOrCheckArtifactFromId(artId, branch, allowDeleted, QueryType.GET, false);
+   public static Artifact getArtifactFromIdOrNull(ArtifactId artId, BranchId branch, DeletionFlag allowDeleted) throws OseeCoreException {
+      return getOrCheckArtifactFromId(artId.getId().intValue(), branch, allowDeleted, QueryType.GET, false);
    }
 
    public static Artifact getArtifactFromId(Long artId, BranchId branch, DeletionFlag allowDeleted) throws OseeCoreException {
@@ -196,6 +196,10 @@ public class ArtifactQuery {
     */
    public static Artifact checkArtifactFromId(long uuid, BranchId branch) throws OseeCoreException {
       return getOrCheckArtifactFromId(new Long(uuid).intValue(), branch, EXCLUDE_DELETED, QueryType.CHECK);
+   }
+
+   public static Artifact checkArtifactFromId(ArtifactId artifactId, BranchId branch) {
+      return getOrCheckArtifactFromId(artifactId.getId().intValue(), branch, EXCLUDE_DELETED, QueryType.CHECK);
    }
 
    /**
