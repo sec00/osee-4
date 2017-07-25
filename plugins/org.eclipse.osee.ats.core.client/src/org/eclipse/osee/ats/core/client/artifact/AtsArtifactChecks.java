eclipse-osee/org.eclipse.osee

feature[ats_TW1161]: Remove ActionableItem from code base
Change-Id: Ieb98864ef689719b3b6a394eea80d89ef475c2df

plugins/org.eclipse.osee.ats.core.client/src/org/eclipse/osee/ats/core/client/artifact/AtsArtifactChecks.java
@@ -31,6 +31,7 @@ import org.eclipse.osee.ats.core.client.search.UserRelatedToAtsObjectSearch;
 import org.eclipse.osee.ats.core.client.util.AtsUtilClient;
 import org.eclipse.osee.ats.core.client.workflow.AbstractWorkflowArtifact;
 import org.eclipse.osee.ats.core.util.AtsUtilCore;
+import org.eclipse.osee.framework.core.data.ArtifactId;
 import org.eclipse.osee.framework.core.data.IRelationType;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
 import org.eclipse.osee.framework.jdk.core.util.Strings;
@@ -157,15 +158,15 @@ public class AtsArtifactChecks extends ArtifactCheck {
    }
 
    private IStatus checkActionableItems(boolean isAtsAdmin, Collection<Artifact> artifacts) throws OseeCoreException {
-      Set<String> aiaGuids = getActionableItemGuidsWithRecurse(new HashSet<String>(), artifacts);
-      if (!aiaGuids.isEmpty()) {
+      Set<ArtifactId> aiIds = getActionableItemIdsWithRecurse(new HashSet<>(), artifacts);
+      if (!aiIds.isEmpty()) {
          List<Artifact> teamWfsRelatedToAis =
             ArtifactQuery.getArtifactListFromTypeAndAttribute(AtsArtifactTypes.TeamWorkflow,
-               AtsAttributeTypes.ActionableItem, aiaGuids, AtsClientService.get().getAtsBranch(), 10);
+               AtsAttributeTypes.ActionableItemReference, aiIds, AtsClientService.get().getAtsBranch());
          if (!teamWfsRelatedToAis.isEmpty()) {
             return createStatus(String.format(
                "Actionable Items (or children AIs) [%s] selected to delete have related Team Workflows; Delete or re-assign Team Workflows first.",
-               aiaGuids));
+               aiIds));
          }
          if (!isAtsAdmin) {
             return createStatus("Deletion of Actionable Items is only permitted by ATS Admin.");
@@ -174,20 +175,20 @@ public class AtsArtifactChecks extends ArtifactCheck {
       return Status.OK_STATUS;
    }
 
-   private Set<String> getActionableItemGuidsWithRecurse(HashSet<String> aiaGuids, Collection<Artifact> artifacts) {
+   private Set<ArtifactId> getActionableItemIdsWithRecurse(HashSet<ArtifactId> aiIds, Collection<Artifact> artifacts) {
       for (Artifact art : artifacts) {
          if (art.isOfType(AtsArtifactTypes.ActionableItem)) {
-            IAtsActionableItem aia = AtsClientService.get().getCache().getAtsObject(art.getId());
-            if (aia != null) {
-               aiaGuids.add(AtsUtilCore.getGuid(aia));
+            IAtsActionableItem ai = AtsClientService.get().getCache().getAtsObject(art.getId());
+            if (ai != null) {
+               aiIds.add(ArtifactId.valueOf(ai));
                Collection<Artifact> childArts = art.getChildren();
-               if (!aia.getChildrenActionableItems().isEmpty()) {
-                  getActionableItemGuidsWithRecurse(aiaGuids, childArts);
+               if (!ai.getChildrenActionableItems().isEmpty()) {
+                  getActionableItemIdsWithRecurse(aiIds, childArts);
                }
             }
          }
       }
-      return aiaGuids;
+      return aiIds;
    }
 
    private IStatus checkTeamDefinitions(boolean isAtsAdmin, Collection<Artifact> artifacts) throws OseeCoreException {

plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/ai/ActionableItemManager.java
@@ -25,7 +25,7 @@ import org.eclipse.osee.ats.api.util.IAtsStoreService;
 import org.eclipse.osee.ats.api.workdef.IAttributeResolver;
 import org.eclipse.osee.ats.core.config.ActionableItems;
 import org.eclipse.osee.ats.core.util.AtsObjects;
-import org.eclipse.osee.ats.core.util.AtsUtilCore;
+import org.eclipse.osee.framework.core.data.ArtifactId;
 import org.eclipse.osee.framework.core.util.Result;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
 import org.eclipse.osee.framework.logging.OseeLog;
@@ -49,11 +49,11 @@ public class ActionableItemManager implements IAtsActionableItemService {
    public Set<IAtsActionableItem> getActionableItems(IAtsObject atsObject) throws OseeCoreException {
       Set<IAtsActionableItem> ais = new HashSet<>();
       if (!atsStoreService.isDeleted(atsObject)) {
-         for (String guid : getActionableItemGuids(atsObject)) {
-            IAtsActionableItem aia = services.getConfigItem(guid);
+         for (ArtifactId id : getActionableItemIds(atsObject)) {
+            IAtsActionableItem aia = services.getConfigItem(id);
             if (aia == null) {
                OseeLog.logf(ActionableItemManager.class, Level.SEVERE,
-                  "Actionable Item Guid [%s] from [%s] doesn't match item in AtsConfigCache", guid,
+                  "Actionable Item Guid [%s] from [%s] doesn't match item in AtsConfigCache", id,
                   atsObject.toStringWithId());
             } else {
                ais.add(aia);
@@ -69,22 +69,20 @@ public class ActionableItemManager implements IAtsActionableItemService {
    }
 
    @Override
-   public Collection<String> getActionableItemGuids(IAtsObject atsObject) throws OseeCoreException {
-      return attrResolver.getAttributesToStringList(atsObject, AtsAttributeTypes.ActionableItem);
+   public Collection<ArtifactId> getActionableItemIds(IAtsObject atsObject) {
+      return attrResolver.getAttributeValues(atsObject, AtsAttributeTypes.ActionableItemReference);
    }
 
    @Override
    public void addActionableItem(IAtsObject atsObject, IAtsActionableItem aia, IAtsChangeSet changes) throws OseeCoreException {
-      String guid = AtsUtilCore.getGuid(aia);
-      if (!getActionableItemGuids(atsObject).contains(guid)) {
-         changes.addAttribute(atsObject, AtsAttributeTypes.ActionableItem, guid);
+      if (!getActionableItemIds(atsObject).contains(atsObject)) {
+         changes.addAttribute(atsObject, AtsAttributeTypes.ActionableItemReference, aia.getStoreObject());
       }
    }
 
    @Override
    public void removeActionableItem(IAtsObject atsObject, IAtsActionableItem aia, IAtsChangeSet changes) throws OseeCoreException {
-      String guid = AtsUtilCore.getGuid(aia);
-      changes.deleteAttribute(atsObject, AtsAttributeTypes.ActionableItem, guid);
+      changes.deleteAttribute(atsObject, AtsAttributeTypes.ActionableItemReference, aia);
    }
 
    @Override
@@ -110,7 +108,7 @@ public class ActionableItemManager implements IAtsActionableItemService {
 
    @Override
    public boolean hasActionableItems(IAtsObject atsObject) {
-      return attrResolver.getAttributeCount(atsObject, AtsAttributeTypes.ActionableItem) > 0;
+      return attrResolver.getAttributeCount(atsObject, AtsAttributeTypes.ActionableItemReference) > 0;
    }
 
    @Override

sec00/osee-4

plugins/org.eclipse.osee.ats.core.client/src/org/eclipse/osee/ats/core/client/artifact/AtsArtifactChecks.java
@@ -157,15 +157,15 @@ public class AtsArtifactChecks extends ArtifactCheck {
    }
 
    private IStatus checkActionableItems(boolean isAtsAdmin, Collection<Artifact> artifacts) throws OseeCoreException {
-      Set<String> aiaGuids = getActionableItemGuidsWithRecurse(new HashSet<String>(), artifacts);
-      if (!aiaGuids.isEmpty()) {
+      Set<String> aiIds = getActionableItemIdsWithRecurse(new HashSet<String>(), artifacts);
+      if (!aiIds.isEmpty()) {
          List<Artifact> teamWfsRelatedToAis =
             ArtifactQuery.getArtifactListFromTypeAndAttribute(AtsArtifactTypes.TeamWorkflow,
-               AtsAttributeTypes.ActionableItem, aiaGuids, AtsClientService.get().getAtsBranch(), 10);
+               AtsAttributeTypes.ActionableItemReference, aiIds, AtsClientService.get().getAtsBranch(), 10);
          if (!teamWfsRelatedToAis.isEmpty()) {
             return createStatus(String.format(
                "Actionable Items (or children AIs) [%s] selected to delete have related Team Workflows; Delete or re-assign Team Workflows first.",
-               aiaGuids));
+               aiIds));
          }
          if (!isAtsAdmin) {
             return createStatus("Deletion of Actionable Items is only permitted by ATS Admin.");
@@ -174,20 +174,20 @@ public class AtsArtifactChecks extends ArtifactCheck {
       return Status.OK_STATUS;
    }
 
-   private Set<String> getActionableItemGuidsWithRecurse(HashSet<String> aiaGuids, Collection<Artifact> artifacts) {
+   private Set<String> getActionableItemIdsWithRecurse(HashSet<String> aiIds, Collection<Artifact> artifacts) {
       for (Artifact art : artifacts) {
          if (art.isOfType(AtsArtifactTypes.ActionableItem)) {
-            IAtsActionableItem aia = AtsClientService.get().getCache().getAtsObject(art.getId());
-            if (aia != null) {
-               aiaGuids.add(AtsUtilCore.getGuid(aia));
+            IAtsActionableItem ai = AtsClientService.get().getCache().getAtsObject(art.getId());
+            if (ai != null) {
+               aiIds.add(ai.getIdString());
                Collection<Artifact> childArts = art.getChildren();
-               if (!aia.getChildrenActionableItems().isEmpty()) {
-                  getActionableItemGuidsWithRecurse(aiaGuids, childArts);
+               if (!ai.getChildrenActionableItems().isEmpty()) {
+                  getActionableItemIdsWithRecurse(aiIds, childArts);
                }
             }
          }
       }
-      return aiaGuids;
+      return aiIds;
    }
 
    private IStatus checkTeamDefinitions(boolean isAtsAdmin, Collection<Artifact> artifacts) throws OseeCoreException {

plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/ai/ActionableItemManager.java
@@ -25,7 +25,6 @@ import org.eclipse.osee.ats.api.util.IAtsStoreService;
 import org.eclipse.osee.ats.api.workdef.IAttributeResolver;
 import org.eclipse.osee.ats.core.config.ActionableItems;
 import org.eclipse.osee.ats.core.util.AtsObjects;
-import org.eclipse.osee.ats.core.util.AtsUtilCore;
 import org.eclipse.osee.framework.core.util.Result;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
 import org.eclipse.osee.framework.logging.OseeLog;
@@ -49,11 +48,11 @@ public class ActionableItemManager implements IAtsActionableItemService {
    public Set<IAtsActionableItem> getActionableItems(IAtsObject atsObject) throws OseeCoreException {
       Set<IAtsActionableItem> ais = new HashSet<>();
       if (!atsStoreService.isDeleted(atsObject)) {
-         for (String guid : getActionableItemGuids(atsObject)) {
-            IAtsActionableItem aia = services.getConfigItem(guid);
+         for (String id : getActionableItemIds(atsObject)) {
+            IAtsActionableItem aia = services.getConfigItem(id);
             if (aia == null) {
                OseeLog.logf(ActionableItemManager.class, Level.SEVERE,
-                  "Actionable Item Guid [%s] from [%s] doesn't match item in AtsConfigCache", guid,
+                  "Actionable Item Guid [%s] from [%s] doesn't match item in AtsConfigCache", id,
                   atsObject.toStringWithId());
             } else {
                ais.add(aia);
@@ -69,22 +68,20 @@ public class ActionableItemManager implements IAtsActionableItemService {
    }
 
    @Override
-   public Collection<String> getActionableItemGuids(IAtsObject atsObject) throws OseeCoreException {
-      return attrResolver.getAttributesToStringList(atsObject, AtsAttributeTypes.ActionableItem);
+   public Collection<String> getActionableItemIds(IAtsObject atsObject) throws OseeCoreException {
+      return attrResolver.getAttributesToStringList(atsObject, AtsAttributeTypes.ActionableItemReference);
    }
 
    @Override
    public void addActionableItem(IAtsObject atsObject, IAtsActionableItem aia, IAtsChangeSet changes) throws OseeCoreException {
-      String guid = AtsUtilCore.getGuid(aia);
-      if (!getActionableItemGuids(atsObject).contains(guid)) {
-         changes.addAttribute(atsObject, AtsAttributeTypes.ActionableItem, guid);
+      if (!getActionableItemIds(atsObject).contains(atsObject.getId())) {
+         changes.addAttribute(atsObject, AtsAttributeTypes.ActionableItemReference, aia.getStoreObject());
       }
    }
 
    @Override
    public void removeActionableItem(IAtsObject atsObject, IAtsActionableItem aia, IAtsChangeSet changes) throws OseeCoreException {
-      String guid = AtsUtilCore.getGuid(aia);
-      changes.deleteAttribute(atsObject, AtsAttributeTypes.ActionableItem, guid);
+      changes.deleteAttribute(atsObject, AtsAttributeTypes.ActionableItemReference, aia.getIdString());
    }
 
    @Override
@@ -110,7 +107,7 @@ public class ActionableItemManager implements IAtsActionableItemService {
 
    @Override
    public boolean hasActionableItems(IAtsObject atsObject) {
-      return attrResolver.getAttributeCount(atsObject, AtsAttributeTypes.ActionableItem) > 0;
+      return attrResolver.getAttributeCount(atsObject, AtsAttributeTypes.ActionableItemReference) > 0;
    }
 
    @Override