eclipse-osee/org.eclipse.osee

feature[ats_TW1384]: Create ATS REST Create
Change-Id: Icb50d0a89b0f9749e79872a61cedf8b51f9a8e0e

plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/config/ConvertAtsConfigGuidAttributesOperation.java
@@ -30,6 +30,7 @@ import org.eclipse.osee.framework.core.data.ArtifactId;
 import org.eclipse.osee.framework.core.data.ArtifactToken;
 import org.eclipse.osee.framework.core.data.AttributeTypeToken;
 import org.eclipse.osee.framework.core.data.IAttribute;
+import org.eclipse.osee.framework.core.data.TransactionId;
 import org.eclipse.osee.framework.core.util.result.XResultData;
 import org.eclipse.osee.framework.jdk.core.util.Collections;
 import org.eclipse.osee.framework.jdk.core.util.Strings;
@@ -79,8 +80,8 @@ public class ConvertAtsConfigGuidAttributesOperation {
             convertActionableItemsIfNeeded(changes, art);
             services.getLogger().error("Work Item - " + art.toStringWithId());
          }
-         boolean changed = changes.executeIfNeeded();
-         if (changed) {
+         TransactionId transaction = changes.executeIfNeeded();
+         if (transaction != null && transaction.isValid()) {
             services.getLogger().info("================================== > executed");
          }
          try {
@@ -106,8 +107,8 @@ public class ConvertAtsConfigGuidAttributesOperation {
          for (ArtifactToken art : allArtifacts) {
             convertWorkPackageIfNeeded(changes, art);
          }
-         boolean changed = changes.executeIfNeeded();
-         if (changed) {
+         TransactionId transaction = changes.executeIfNeeded();
+         if (transaction != null && transaction.isValid()) {
             services.getLogger().info("================================== > executed");
          }
          try {

sec00/osee-4

plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/config/ConvertAtsConfigGuidAttributesOperation.java
@@ -30,6 +30,7 @@ import org.eclipse.osee.framework.core.data.ArtifactId;
 import org.eclipse.osee.framework.core.data.ArtifactToken;
 import org.eclipse.osee.framework.core.data.AttributeTypeToken;
 import org.eclipse.osee.framework.core.data.IAttribute;
+import org.eclipse.osee.framework.core.data.TransactionId;
 import org.eclipse.osee.framework.core.util.result.XResultData;
 import org.eclipse.osee.framework.jdk.core.util.Collections;
 import org.eclipse.osee.framework.jdk.core.util.Strings;
@@ -79,8 +80,8 @@ public class ConvertAtsConfigGuidAttributesOperation {
             convertActionableItemsIfNeeded(changes, art);
             services.getLogger().error("Work Item - " + art.toStringWithId());
          }
-         boolean changed = changes.executeIfNeeded();
-         if (changed) {
+         TransactionId changed = changes.executeIfNeeded();
+         if (changed.isValid()) {
             services.getLogger().info("================================== > executed");
          }
          try {
@@ -106,8 +107,8 @@ public class ConvertAtsConfigGuidAttributesOperation {
          for (ArtifactToken art : allArtifacts) {
             convertWorkPackageIfNeeded(changes, art);
          }
-         boolean changed = changes.executeIfNeeded();
-         if (changed) {
+         TransactionId changed = changes.executeIfNeeded();
+         if (changed.isValid()) {
             services.getLogger().info("================================== > executed");
          }
          try {