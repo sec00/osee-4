8ff2c9e61a6f438151e0d4a28e6d17b334c6081c
donald.g.dunne <donald.g.dunne@boeing.com>
2018-02-07 14:23:36 -0700

    feature[ats_TW7017]: Convert Work Definition attribute to ArtifactRefAttribute
    Change-Id: I2cd77d8e7dd6aa20044c478d4fe32f2e1abd881c

--- plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/util/AtsChangeSet.java
+++ plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/util/AtsChangeSet.java
@@ -102,6 +102,11 @@ public class AtsChangeSet extends AbstractAtsChangeSet {
          listener.changesStored(this);
       }
       notifier.sendNotifications(getNotifications());
+      for (IAtsObject atsObject : new ArrayList<>(atsObjects)) {
+         if (atsObject instanceof IAtsWorkItem) {
+            atsApi.getWorkDefinitionService().internalClearWorkDefinition((IAtsWorkItem) atsObject);
+         }
+      }
 
       /**
        * Commented out on 0.25.0 due to performance issues; No users are using this feature. Will be re-enabled on
