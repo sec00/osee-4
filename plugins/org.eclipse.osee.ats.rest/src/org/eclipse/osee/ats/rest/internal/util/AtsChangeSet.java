eclipse-osee/org.eclipse.osee

bug[ats_TW10547]: Intermittent test failure in ATS peer review
Change-Id: I1df4caf9f6fa48cbe206e120fdc29d34d042de0a

plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/util/AtsChangeSet.java
@@ -101,6 +101,11 @@ public class AtsChangeSet extends AbstractAtsChangeSet {
          listener.changesStored(this);
       }
       notifier.sendNotifications(getNotifications());
+      for (IAtsObject atsObject : new ArrayList<>(atsObjects)) {
+         if (atsObject instanceof IAtsWorkItem) {
+            atsServer.getStoreService().clearCaches((IAtsWorkItem) atsObject);
+         }
+      }
 
       /**
        * Commented out on 0.25.0 due to performance issues; No users are using this feature. Will be re-enabled on

sec00/osee-4

plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/util/AtsChangeSet.java
@@ -103,7 +103,7 @@ public class AtsChangeSet extends AbstractAtsChangeSet {
       notifier.sendNotifications(getNotifications());
       for (IAtsObject atsObject : new ArrayList<>(atsObjects)) {
          if (atsObject instanceof IAtsWorkItem) {
-            atsApi.getWorkDefinitionService().internalClearWorkDefinition((IAtsWorkItem) atsObject);
+            atsApi.getStoreService().clearCaches((IAtsWorkItem) atsObject);
          }
       }
 